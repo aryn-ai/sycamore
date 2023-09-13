2184cf7573afe3d3e533c5a63844b376c70d1ce5
Parth Parmar <parth@aryn.ai>
2023-09-13 12:24:41 -0700

    Setup sphinx for sycamore docs

--- sycamore/execution/functions/elements.py
+++ sycamore/execution/functions/elements.py
@@ -8,6 +8,12 @@ def reorder_elements(
     document: Document,
     comparator: Callable[[Element, Element], int],
 ):
+    """
+           Reorders the elements.
+           Args:
+               document: Document for which the elements need to be re-ordered
+               comparator: A comparator function
+        """
     elements = document.elements
     elements.sort(key=functools.cmp_to_key(comparator))
     return document

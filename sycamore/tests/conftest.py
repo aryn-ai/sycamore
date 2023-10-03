014563c150de5fbdc90f917a31e096ce05dd5457
Alex Meyer <alex@aryn.ai>
2023-10-03 14:48:15 -0700

    Added unit test to make sure path is inherited.

--- sycamore/tests/conftest.py
+++ sycamore/tests/conftest.py
@@ -7,7 +7,9 @@ from sycamore.data.document import Document
 @pytest.fixture
 def read_local_binary(request) -> Document:
     local = LocalFileSystem()
-    input_stream = local.open_input_stream(str(request.param))
+    path = str(request.param)
+    input_stream = local.open_input_stream(path)
     document = Document()
     document.binary_representation = input_stream.readall()
+    document.properties["path"] = path
     return document

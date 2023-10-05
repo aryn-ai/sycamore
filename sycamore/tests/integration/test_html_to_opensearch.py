a84f61f1a700364e463bf8f8a8d338cbb8708130
Parth Parmar <parth@aryn.ai>
2023-10-05 10:54:13 -0700

    Integ Test

--- sycamore/tests/integration/test_html_to_opensearch.py
+++ sycamore/tests/integration/test_html_to_opensearch.py
@@ -38,7 +38,6 @@ def test_html_to_opensearch():
     ds = (
         context.read.binary(paths, binary_format="html")
         .partition(partitioner=HtmlPartitioner())
-        .filter()
         .explode()
         .embed(SentenceTransformerEmbedder(batch_size=100, model_name="sentence-transformers/all-MiniLM-L6-v2"))
     )

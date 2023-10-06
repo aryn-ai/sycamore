25c007b12328a81d4c5f53e527a4922565db4c39
Ben Sowell <ben@aryn.ai>
2023-10-05 17:22:27 -0700

    Add a FileWriter implementation.
    This can be used to write a DocSet out to a filesystem. The method for
    converting a Document into bytes and choosing a filename are
    customizable for different use cases.

--- sycamore/tests/unit/test_writer.py
+++ sycamore/tests/unit/test_writer.py
@@ -1,6 +1,53 @@
 from sycamore import DocSet, Context
+import sycamore
+from sycamore.data import Document
 from sycamore.plan_nodes import Node
 from sycamore.writers import OpenSearchWriter
+
+from pathlib import Path
+
+from sycamore.writers.file_writer import default_filename, default_doc_to_bytes, json_properties_content
+
+
+def generate_docs(num: int, type: str = "test", text=True, binary=False) -> list[Document]:
+    docs = []
+    for i in range(num):
+        doc = Document(
+            {
+                "doc_id": f"doc_{i}",
+                "type": "test",
+                "elements": {"array": []},
+                "properties": {"filename": f"doc_{i}.dat"},
+            }
+        )
+
+        if text:
+            doc["text_representation"] = f"This is text content {i}"
+        if binary:
+            doc["binary_representation"] = f"This is binary content {i}".encode("utf-8")
+
+        docs.append(doc)
+
+    return docs
+
+
+def _check_doc_path(
+    docs: list[Document], path: Path, filename_fn=default_filename, doc_to_bytes_fn=default_doc_to_bytes
+):
+    docs_by_filename = {filename_fn(doc): doc for doc in docs}
+    paths = [p for p in path.iterdir() if p.is_file()]
+    assert len(paths) == len(docs)
+
+    for p in paths:
+        with open(p, "rb") as infile:
+            expected = doc_to_bytes_fn(docs_by_filename[p.name])
+            actual = infile.read()
+
+            assert expected == actual
+
+
+def _test_filename(doc: Document) -> str:
+    return doc.properties["filename"]
 
 
 class TestDocSetWriter:
@@ -10,3 +57,31 @@ class TestDocSetWriter:
         execute = mocker.patch.object(OpenSearchWriter, "execute")
         docset.write.opensearch(os_client_args={}, index_name="index")
         execute.assert_called_once()
+
+    def test_file_writer_text(self, tmp_path: Path):
+        docs = generate_docs(5)
+        context = sycamore.init()
+        doc_set = context.read.document(docs)
+        doc_set.write.files(str(tmp_path))
+        _check_doc_path(docs, tmp_path)
+
+    def test_file_writer_binary(self, tmp_path: Path):
+        docs = generate_docs(5, text=False, binary=True)
+        context = sycamore.init()
+        doc_set = context.read.document(docs)
+        doc_set.write.files(str(tmp_path))
+        _check_doc_path(docs, tmp_path)
+
+    def test_file_writer_filename_fn(self, tmp_path: Path):
+        docs = generate_docs(5)
+        context = sycamore.init()
+        doc_set = context.read.document(docs)
+        doc_set.write.files(str(tmp_path), filename_fn=_test_filename)
+        _check_doc_path(docs, tmp_path, filename_fn=_test_filename)
+
+    def test_file_writer_doc_to_bytes_fn(self, tmp_path: Path):
+        docs = generate_docs(5)
+        context = sycamore.init()
+        doc_set = context.read.document(docs)
+        doc_set.write.files(str(tmp_path), doc_to_bytes_fn=json_properties_content)
+        _check_doc_path(docs, tmp_path, doc_to_bytes_fn=json_properties_content)
--- sycamore/writer.py
+++ sycamore/writer.py
@@ -1,7 +1,11 @@
-from typing import Optional
+from typing import Callable, Optional
+
+from pyarrow.fs import FileSystem
 
 from sycamore import Context
 from sycamore.plan_nodes import Node
+from sycamore.data import Document
+from sycamore.writers.file_writer import default_doc_to_bytes, default_filename, FileWriter
 
 
 class DocSetWriter:
@@ -18,3 +22,34 @@ class DocSetWriter:
             self.plan, index_name, os_client_args=os_client_args, index_settings=index_settings, **resource_args
         )
         os.execute()
+
+    def files(
+        self,
+        path: str,
+        filesystem: Optional[FileSystem] = None,
+        filename_fn: Callable[[Document], str] = default_filename,
+        doc_to_bytes_fn: Callable[[Document], bytes] = default_doc_to_bytes,
+        **resource_args
+    ) -> None:
+        """Writes the content of each Document to a separate file.
+
+        Args:
+            path: The path prefix to write to. Should include the scheme if not local.
+            filesystem: The pyarrow.fs FileSystem to use.
+            filename_fn: A function for generating a file name. Takes a Document
+                and returns a unique name that will be appended to path.
+            doc_to_bytes_fn: A function from a Document to bytes for generating the data to write.
+                Defaults to using text_representation if available, or binary_representation
+                if not.
+            resource_args: Arguments to pass to the underlying execution environment.
+        """
+        file_writer = FileWriter(
+            self.plan,
+            path,
+            filesystem=filesystem,
+            filename_fn=filename_fn,
+            doc_to_bytes_fn=doc_to_bytes_fn,
+            **resource_args
+        )
+
+        file_writer.execute()

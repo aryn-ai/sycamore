55e489225c3d4b9ae8aa901ba6e59e515bfb500e
HenryL27 <hmlindeman@yahoo.com>
2023-10-17 12:12:34 -0700

    add merge_elements transform
    Signed-off-by: HenryL27 <hmlindeman@yahoo.com>

---
+++ sycamore/tests/unit/transforms/test_merge_elements.py
@@ -0,0 +1,111 @@
+
+from transformers import AutoTokenizer
+
+from sycamore.data import Document, Element
+from sycamore.functions import filter_elements
+from sycamore.transforms.merge_elements import GreedyElementMerger
+
+
+class TestMergeElements:
+    dict0 = {
+        "doc_id": "doc_id",
+        "type": "pdf",
+        "text_representation": "text",
+        "binary_representation": None,
+        "parent_id": None,
+        "properties": {"path": "/docs/foo.txt", "title": "bar"},
+        "elements": {
+            "array": [
+                {
+                    "type": "UncategorizedText",
+                    "text_representation": "text1",
+                    "properties": {"filetype": "text/plain", "page_number": 1},
+                },
+                {
+                    "type": "UncategorizedText",
+                    "text_representation": "text2",
+                    "properties": {"filetype": "text/plain", "page_number": 2},
+                },
+                {
+                    "type": "Title",
+                    "text_representation": "title1",
+                    "bbox": [0.17606372549019608,
+                            0.4045761636363636,
+                            0.8238071275686271,
+                            0.47917035555555554],
+                    "binary_representation": b'title1',
+                    "properties": {"doc_title": "title"}
+                },
+                {
+                    "type": "NarrativeText",
+                    "text_representation": """Recurrent neural networks, long short-term memory [12] and gated recurrent [7] neural networks in particular, have been ﬁrmly established as state of the art approaches in sequence modeling and transduction problems such as language modeling and machine translation [29, 2, 5]. Numerous efforts have since continued to push the boundaries of recurrent language models and encoder-decoder architectures [31, 21, 13].
+                                            ∗Equal contribution. Listing order is random. Jakob proposed replacing RNNs with self-attention and started the effort to evaluate this idea. Ashish, with Illia, designed and implemented the ﬁrst Transformer models and has been crucially involved in every aspect of this work. Noam proposed scaled dot-product attention, multi-head attention and the parameter-free position representation and became the other person involved in nearly every detail. Niki designed, implemented, tuned and evaluated countless model variants in our original codebase and tensor2tensor. Llion also experimented with novel model variants, was responsible for our initial codebase, and efﬁcient inference and visualizations. Lukasz and Aidan spent countless long days designing various parts of and implementing tensor2tensor, replacing our earlier codebase, greatly improving results and massively accelerating our research.
+                                            †Work performed while at Google Brain. ‡Work performed while at Google Research.
+                                            31st Conference on Neural Information Processing Systems (NIPS 2017), Long Beach, CA, USA.""",
+                    "bbox": [0.27606372549019608,
+                            0.5045761636363636,
+                            0.9238071275686271,
+                            0.67917035555555554],
+                    "properties": {"doc_title": "other title", "prop2": "prop 2 value"}
+                },
+                {
+                    "type": "NarrativeText",
+                    "text_representation": """Recurrent neural networks, long short-term memory [12] and gated recurrent [7] neural networks in particular, have been ﬁrmly established as state of the art approaches in sequence modeling and transduction problems such as language modeling and machine translation [29, 2, 5]. Numerous efforts have since continued to push the boundaries of recurrent language models and encoder-decoder architectures [31, 21, 13].
+                                            ∗Equal contribution. Listing order is random. Jakob proposed replacing RNNs with self-attention and started the effort to evaluate this idea. Ashish, with Illia, designed and implemented the ﬁrst Transformer models and has been crucially involved in every aspect of this work. Noam proposed scaled dot-product attention, multi-head attention and the parameter-free position representation and became the other person involved in nearly every detail. Niki designed, implemented, tuned and evaluated countless model variants in our original codebase and tensor2tensor. Llion also experimented with novel model variants, was responsible for our initial codebase, and efﬁcient inference and visualizations. Lukasz and Aidan spent countless long days designing various parts of and implementing tensor2tensor, replacing our earlier codebase, greatly improving results and massively accelerating our research.
+                                            †Work performed while at Google Brain. ‡Work performed while at Google Research.
+                                            31st Conference on Neural Information Processing Systems (NIPS 2017), Long Beach, CA, USA.""",
+                    "bbox": [0.27606372549019608,
+                            0.5045761636363636,
+                            0.9238071275686271,
+                            0.67917035555555554],
+                    "properties": {"doc_title": "other title", "prop2": "prop 2 value"}
+                },
+                {
+                    "type": "Title",
+                    "text_representation": "title1",
+                    "bbox": [0.17606372549019608,
+                            0.4045761636363636,
+                            0.8238071275686271,
+                            0.47917035555555554],
+                    "binary_representation": b'title1',
+                    "properties": {"doc_title": "title"}
+                },
+                {
+                    
+                }
+            ]
+        },
+    }
+
+    def test_merge_elements(self):
+        doc = Document(self.dict0)
+        tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
+        merger = GreedyElementMerger(tokenizer, 512)
+        new_doc = merger.merge_elements(doc)
+        assert len(new_doc.elements) == 4
+
+        e = new_doc.elements[0]
+        assert e.type == "Section"
+        assert e.text_representation == "text1\ntext2"
+        assert e.properties == {"filetype": "text/plain", "page_number": 1}
+
+        e = new_doc.elements[1]
+        assert e.type == "Section"
+        assert e.bbox.coordinates == (0.17606372549019608,
+                                        0.4045761636363636,
+                                        0.9238071275686271,
+                                        0.67917035555555554)
+        assert e.properties == {"doc_title": "title", "prop2": "prop 2 value"}
+        
+        e = new_doc.elements[2]
+        assert e.type == "NarrativeText"
+        
+        e = new_doc.elements[3]
+        assert e.type == "Section"
+        assert e.text_representation == "title1"
+        assert e.bbox.coordinates == (0.17606372549019608,
+                                        0.4045761636363636,
+                                        0.8238071275686271,
+                                        0.47917035555555554)
+        assert e.binary_representation == b'title1'
+        assert e.properties == {"doc_title": "title"}
---
+++ sycamore/transforms/merge_elements.py
@@ -0,0 +1,130 @@
+from abc import ABC, abstractmethod
+
+from ray.data import Dataset
+
+from sycamore.data import Document, Element, BoundingBox
+from sycamore.plan_nodes import NonCPUUser, NonGPUUser, Transform, Node
+from sycamore.transforms.map import generate_map_function
+
+from typing import Tuple
+
+
+
+class ElementMerger(ABC):
+    @abstractmethod
+    def merge_elements(self, document: Document) -> Document:
+        pass
+
+class GreedyElementMerger(ElementMerger):
+
+    def __init__(
+        self,
+        tokenizer,
+        max_tokens
+    ):
+        self.tokenizer = tokenizer
+        self.max_tokens = max_tokens
+
+
+    def _should_merge(self, element1: Tuple[Element, int], element2: Tuple[Element, int]) -> bool:
+        if element2[0].type == 'Title':
+            return False
+        if element1[1] + 1 + element2[1] > self.max_tokens:
+            return False
+        return True
+
+    def _merge(self, element1: Tuple[Element, int], element2: Tuple[Element, int]) -> Tuple[Element, int]:
+        """
+        Merge two elements; the new element's fields will be set as:
+        
+            type: "Section"
+            binary_representation: elt1.binary_representation + elt2.binary_representation
+            text_representation: elt1.text_representation + '\n' + elt2.text_representation
+            bbox: the minimal bbox that contains both elt1's and elt2's bboxes
+            properties: elt1's properties + any of elt2's properties that are not in elt1
+                > note: if elt1 and elt2 have different values for the same property, we take elt1's value
+            
+            > note: if any input field is None we take the other element's field without merge logic
+
+        Args:
+            element1 (Tuple[Element, int]): the first element (and number of tokens in it)
+            element2 (Tuple[Element, int]): the second element (and number of tokens in it)
+
+        Returns:
+            Tuple[Element, int]: a new merged element from the inputs (and number of tokens in it)
+        """
+        elt1, tok1 = element1
+        elt2, tok2 = element2
+        new_toks = 0
+        new_elt = Element()
+        new_elt.type = "Section"
+        # Merge binary representations by concatenation
+        if elt1.binary_representation == None or elt2.binary_representation == None:
+            new_elt.binary_representation = elt1.binary_representation or elt2.binary_representation
+        else:
+            new_elt.binary_representation = elt1.binary_representation + elt2.binary_representation
+        # Merge text representations by concatenation with a newline
+        if elt1.text_representation == None or elt2.text_representation == None:
+            new_elt.text_representation = elt1.text_representation or elt2.text_representation
+            new_toks = max(tok1, tok2)
+        else:
+            new_elt.text_representation = elt1.text_representation + '\n' + elt2.text_representation
+            new_toks = tok1 + 1 + tok2
+        # Merge bbox by taking the coords that make the largest box
+        if elt1.bbox == None and elt2.bbox == None:
+            pass
+        elif elt1.bbox == None or elt2.bbox == None:
+            new_elt.bbox = elt1.bbox or elt2.bbox
+        else:
+            new_elt.bbox = BoundingBox(
+                min(elt1.bbox.x1, elt2.bbox.x1),
+                min(elt1.bbox.y1, elt2.bbox.y1),
+                max(elt1.bbox.x2, elt2.bbox.x2),
+                max(elt1.bbox.y2, elt2.bbox.y2)
+            )
+        # Merge properties by taking the union of the keys
+        for k, v in elt1.properties.items():
+            new_elt.properties[k] = v
+        for k, v in elt2.properties.items():
+            if new_elt.properties.get(k) is None:
+                new_elt.properties[k] = v
+
+        return (new_elt, new_toks)
+        
+        
+
+    def merge_elements(self, document: Document) -> Document:
+        """Use self._should_merge and self._merge to greedily merge consecutive elements.
+        If the next element should be merged into the last 'accumulation' element, merge it.
+
+        Args:
+            document (Document): A document with elements to be merged. 
+
+        Returns:
+            Document: The same document, with its elements merged
+        """
+        if len(document.elements) < 2:
+            return document
+        token_counts = [len(self.tokenizer.encode(e.text_representation or "")) for e in document.elements]
+        new_elts = [(document.elements[0], token_counts[0])]
+        for element, tokens in zip(document.elements[1:], token_counts[1:]):
+            if self._should_merge(new_elts[-1], (element, tokens)):
+                new_elts[-1] = self._merge(new_elts[-1], (element, tokens))
+            else:
+                new_elts.append((element, tokens))
+        document.elements = [x[0] for x in new_elts]
+        return document
+
+class Merge(NonCPUUser, NonGPUUser, Transform):
+    """
+    Merge Elements into fewer large elements
+    """
+
+    def __init__(self, child: Node, merger: ElementMerger, **kwargs):
+        super().__init__(child, **kwargs)
+        self._merger = merger
+
+    def execute(self) -> Dataset:
+        input_dataset = self.child().execute()
+        dataset = input_dataset.map(generate_map_function(self._merger.merge_elements))
+        return dataset

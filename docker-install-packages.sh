c012643eb685075aa0631fd1a4258db04d184f99
Eric Anderson <eric@aryn.ai>
2023-10-26 23:11:21 -0700

    Working on MacOS when built on MacOS.
    Also fixed importer to only override host when running in container.

--- docker-install-packages.sh
+++ docker-install-packages.sh
@@ -5,6 +5,6 @@ set -x
 export DEBIAN_FRONTEND=noninteractive
 
 apt update
-apt -y install --no-install-recommends poppler-utils
+apt -y install --no-install-recommends poppler-utils python3-poetry gcc python3-dev
 apt clean
 rm -rf /var/lib/apt/lists/*

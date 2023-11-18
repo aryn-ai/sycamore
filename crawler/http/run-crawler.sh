5871fb87b711cad3c521c6e18e4cb8c92568faa9
Eric Anderson <eric@aryn.ai>
2023-11-17 16:43:21 -0800

    Fixup dockerization -- skip sycamore library & add build-stamps (#175)
    Avoid installing the sycamore library because it is large (GB) and unneeded.
    Add the build stamp environment variables and logging we're adding to the other containers.

--- crawler/http/run-crawler.sh
+++ crawler/http/run-crawler.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+echo "Version-Info, Sycamore Crawler HTTP Branch: ${GIT_BRANCH}"
+echo "Version-Info, Sycamore Crawler HTTP Commit: ${GIT_COMMIT}"
+echo "Version-Info, Sycamore Crawler HTTP Diff: ${GIT_DIFF}"
+
 poetry run scrapy crawl sycamore "$@"

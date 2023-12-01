66084e1d1f435828ed87eb53f6b7da2319d04dec
Alex Meyer <144723289+alexaryn@users.noreply.github.com>
2023-12-01 15:08:13 -0800

    Added HTML ingest example.  Fixed order in S3 ingester. (#188)
    * Added HTML ingest example.  Fixed order in S3 ingester.
    
    * Uncomment.
    
    * Bump up chunk size.

--- examples/html_ingest.py
+++ examples/html_ingest.py
@@ -1,15 +1,14 @@
 import sys
-import boto3
-import pyarrow.fs
 
 # ruff: noqa: E402
 sys.path.append("../sycamore")
 
 import sycamore
+from sycamore.functions import TextOverlapChunker
 from sycamore.functions.tokenizer import HuggingFaceTokenizer
 from sycamore.llms import OpenAIModels, OpenAI
 from sycamore.transforms.merge_elements import GreedyTextElementMerger
-from sycamore.transforms.partition import UnstructuredPdfPartitioner
+from sycamore.transforms.partition import HtmlPartitioner
 from sycamore.transforms.extract_entity import OpenAIEntityExtractor
 from sycamore.transforms.embed import SentenceTransformerEmbedder
 
@@ -17,18 +16,9 @@ from simple_config import idx_settings, osrch_args, title_template
 
 paths = sys.argv[1:]
 if not paths:
-    raise RuntimeError("No S3 URLs supplied.")
+    raise RuntimeError("No paths supplied.")
 
 index = "demoindex0"
-
-sess = boto3.session.Session()
-cred = sess.get_credentials()
-fsys = pyarrow.fs.S3FileSystem(
-    access_key=cred.access_key,
-    secret_key=cred.secret_key,
-    region=sess.region_name,
-    session_token=cred.token,
-)
 
 davinci_llm = OpenAI(OpenAIModels.TEXT_DAVINCI.value)
 tokenizer = HuggingFaceTokenizer("thenlper/gte-small")
@@ -36,11 +26,15 @@ tokenizer = HuggingFaceTokenizer("thenlper/gte-small")
 ctx = sycamore.init()
 
 ds = (
-    ctx.read.binary(paths, binary_format="pdf", filesystem=fsys)
-    .partition(partitioner=UnstructuredPdfPartitioner())
+    ctx.read.binary(paths, binary_format="html")
+    .partition(partitioner=HtmlPartitioner(
+        extract_tables=True,
+        text_chunker=TextOverlapChunker(chunk_token_count=1200, chunk_overlap_token_count=120),
+    ))
     .extract_entity(entity_extractor=OpenAIEntityExtractor("title", llm=davinci_llm, prompt_template=title_template))
     .merge(merger=GreedyTextElementMerger(tokenizer=tokenizer, max_tokens=512))
     .spread_properties(["path", "title"])
+    
     .explode()
     .embed(embedder=SentenceTransformerEmbedder(model_name="thenlper/gte-small", batch_size=100))
 )

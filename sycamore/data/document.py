538e9d399089df8019c2d9a200b145e0bdceca02
Eric Anderson <eric@aryn.ai>
2023-12-19 16:58:16 -0800

    Add docstrings to all public document methods so they show up on sycamore.readthedocs.io

--- sycamore/data/document.py
+++ sycamore/data/document.py
@@ -24,36 +24,43 @@ class Document(UserDict):
 
     @doc_id.setter
     def doc_id(self, value: str) -> None:
+        """Set the unique identifier of the document."""
         self.data["doc_id"] = value
 
     @property
     def type(self) -> Optional[str]:
+        """The type of the document, e.g. pdf, html."""
         return self.data.get("type")
 
     @type.setter
     def type(self, value: str) -> None:
+        """Set the type of the document."""
         self.data["type"] = value
 
     @property
     def text_representation(self) -> Optional[str]:
+        """The text representation of the document."""
         return self.data.get("text_representation")
 
     @text_representation.setter
     def text_representation(self, value: str) -> None:
+        """Set the text representation of the document."""
         self.data["text_representation"] = value
 
     @property
     def binary_representation(self) -> Optional[bytes]:
-        """The raw content of the document in stored in the appropriate format.For example, the
+        """The raw content of the document stored in the appropriate format. For example, the
         content of a PDF document will be stored as the binary_representation."""
         return self.data.get("binary_representation")
 
     @binary_representation.setter
     def binary_representation(self, value: bytes) -> None:
+        """Set the raw content of the document."""
         self.data["binary_representation"] = value
 
     @binary_representation.deleter
     def binary_representation(self) -> None:
+        """Delete the raw content of the document."""
         self.data["binary_representation"] = None
 
     @property
@@ -64,18 +71,22 @@ class Document(UserDict):
 
     @elements.setter
     def elements(self, elements: list[Element]):
+        """Set the elements for this document."""
         self.data["elements"] = [element.data for element in elements]
 
     @elements.deleter
     def elements(self) -> None:
+        """Delete the elements of this document."""
         self.data["elements"] = []
 
     @property
     def embedding(self) -> Optional[list[float]]:
+        """Get the embedding for this document."""
         return self.data.get("embedding")
 
     @embedding.setter
     def embedding(self, embedding: list[float]) -> None:
+        """Set the embedding for this document."""
         self.data["embedding"] = embedding
 
     @property
@@ -88,14 +99,17 @@ class Document(UserDict):
 
     @parent_id.setter
     def parent_id(self, value: str) -> None:
+        """Set the parent_id for this document."""
         self.data["parent_id"] = value
 
     @property
     def bbox(self) -> Optional[BoundingBox]:
+        """Get the bounding box for this document."""
         return None if self.data.get("bbox") is None else BoundingBox(*self.data["bbox"])
 
     @bbox.setter
     def bbox(self, bbox: BoundingBox) -> None:
+        """Set the bounding box for this document."""
         self.data["bbox"] = bbox.coordinates
 
     @property
@@ -106,26 +120,32 @@ class Document(UserDict):
 
     @properties.setter
     def properties(self, properties: dict[str, Any]):
+        """Set all the proprites for this document."""
         self.data["properties"] = properties
 
     @properties.deleter
     def properties(self) -> None:
+        """Delete all the properties of this document."""
         self.data["properties"] = {}
 
     def serialize(self) -> bytes:
+        """Serialize this document to bytes."""
         from pickle import dumps
 
         return dumps(self.data)
 
     @staticmethod
     def deserialize(raw: bytes) -> "Document":
+        """Unserialize from bytes to a Document."""
         from pickle import loads
 
         return Document(loads(raw))
 
     @staticmethod
     def from_row(row: dict[str, bytes]) -> "Document":
+        """Unserialize a Ray row back into a Document."""
         return Document(row["doc"])
 
     def to_row(self) -> dict[str, bytes]:
+        """Serialize this document into a row for use with Ray."""
         return {"doc": self.serialize()}

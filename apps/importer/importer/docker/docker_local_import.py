c3e64d5596d2b35929f406589e44ffe8d72ba96c
HenryL27 <hmlindeman@yahoo.com>
2024-03-19 14:02:36 -0700

    move immporter -> apps/importer
    Signed-off-by: HenryL27 <hmlindeman@yahoo.com>

--- apps/importer/importer/docker/docker_local_import.py
+++ apps/importer/importer/docker/docker_local_import.py
@@ -9,7 +9,7 @@ import sys
 import time
 
 # ruff: noqa: E402
-sys.path.append("../sycamore")
+# sys.path.append("../sycamore")
 sys.path.append("/app")
 
 import sycamore
@@ -24,7 +24,9 @@ from sycamore.transforms.partition import UnstructuredPdfPartitioner, HtmlPartit
 # from simple_config import idx_settings, osrch_args, title_template
 
 running_in_container = False
-enable_textract = "SYCAMORE_TEXTRACT_PREFIX" in os.environ and os.environ["SYCAMORE_TEXTRACT_PREFIX"]
+enable_textract = (
+    "SYCAMORE_TEXTRACT_PREFIX" in os.environ and os.environ["SYCAMORE_TEXTRACT_PREFIX"]
+)
 index = "demoindex0"
 
 
@@ -44,13 +46,21 @@ def main():
     root_path = "/app/.scrapy"
     if len(sys.argv) <= 1:
         if not os.path.isdir(root_path):
-            raise RuntimeError("Missing " + root_path + "; run with single path argument if not in container")
+            raise RuntimeError(
+                "Missing "
+                + root_path
+                + "; run with single path argument if not in container"
+            )
     elif len(sys.argv) != 2:
         raise RuntimeError("Usage: docker_local_injest.py [directory_root]")
     else:
         root_path = sys.argv[1]
         if not os.path.isdir(root_path):
-            raise RuntimeError("Missing specified path " + root_path + "; correct argument or remove if in container")
+            raise RuntimeError(
+                "Missing specified path "
+                + root_path
+                + "; correct argument or remove if in container"
+            )
 
     global running_in_container
     if root_path == "/app/.scrapy":
@@ -71,13 +81,19 @@ def main():
 
     if enable_textract:
         if "AWS_ACCESS_KEY_ID" not in os.environ:
-            raise RuntimeError("Missing AWS_ACCESS_KEY_ID; or you can export ENABLE_TEXTRACT=false")
+            raise RuntimeError(
+                "Missing AWS_ACCESS_KEY_ID; or you can export ENABLE_TEXTRACT=false"
+            )
 
         if "AWS_SECRET_ACCESS_KEY" not in os.environ:
-            raise RuntimeError("missing AWS_SECRET_ACCESS_KEY; or you can export ENABLE_TEXTRACT=false")
+            raise RuntimeError(
+                "missing AWS_SECRET_ACCESS_KEY; or you can export ENABLE_TEXTRACT=false"
+            )
 
         if "AWS_SESSION_TOKEN" not in os.environ:
-            print("WARNING: AWS_SESSION_TOKEN not present; secret key may not work if it is a short term sso token")
+            print(
+                "WARNING: AWS_SESSION_TOKEN not present; secret key may not work if it is a short term sso token"
+            )
         # TODO: https://github.com/aryn-ai/quickstart/issues/1 - check for AWS_CREDENTIAL_EXIRATION
 
     root = Path(root_path)
@@ -89,7 +105,9 @@ def main():
     # Make sure that we spend ~75% of the time running at full parallelism 75% because the tasks
     # should finish in 4 units, and the first 3 of those will be at full parallelism.
     files_per_run_limit = math.floor(ray_tasks * 4)
-    if ray_tasks == 1:  # If we only have 1 tasks in parallel, no point in doing more than 1 task/run
+    if (
+        ray_tasks == 1
+    ):  # If we only have 1 tasks in parallel, no point in doing more than 1 task/run
         files_per_run_limit = 1
 
     while True:
@@ -97,8 +115,16 @@ def main():
         if len(files) > 0:
             try:
                 if len(files) > max_files_per_run:
-                    print("Have {} remaining files, too many to process in one run.".format(len(files)))
-                    print("Limiting number of files in single run to {}".format(max_files_per_run))
+                    print(
+                        "Have {} remaining files, too many to process in one run.".format(
+                            len(files)
+                        )
+                    )
+                    print(
+                        "Limiting number of files in single run to {}".format(
+                            max_files_per_run
+                        )
+                    )
                     numpy.random.shuffle(files)
                     files = files[0:max_files_per_run]
                 print("Files:", [str(f["path"]) for f in files], flush=True)
@@ -111,7 +137,11 @@ def main():
                     failures = failures - 1
                 else:
                     max_files_per_run = min(max_files_per_run * 2, files_per_run_limit)
-                print("Successfully imported:", [str(f["path"]) for f in files], flush=True)
+                print(
+                    "Successfully imported:",
+                    [str(f["path"]) for f in files],
+                    flush=True,
+                )
 
                 print(
                     "Successful run adjusted failure count to {} and max_files_per_run to {}".format(
@@ -135,7 +165,9 @@ def main():
                 print("WARNING: exception args:", e.args)
                 print("WARNING: exception message:", e)
                 sleep_time = min(10**failures, 300)
-                print("WARNING: sleep(" + str(sleep_time) + ") in case this is persistent")
+                print(
+                    "WARNING: sleep(" + str(sleep_time) + ") in case this is persistent"
+                )
                 time.sleep(sleep_time)
         else:
             print("No changes at", datetime.datetime.now(), "sleeping", flush=True)
@@ -146,19 +178,31 @@ def get_ray_task_count():
     mem_bytes = os.sysconf("SC_PAGE_SIZE") * os.sysconf("SC_PHYS_PAGES")
     usable_mem_bytes = 0.5 * mem_bytes  # use at most half of hosts RAM
     gib = 1024 * 1024 * 1024
-    bytes_per_task = 2 * gib  # Importing sort benchmark varies between 1-2GB while running
+    bytes_per_task = (
+        2 * gib
+    )  # Importing sort benchmark varies between 1-2GB while running
     ray_tasks = math.floor(usable_mem_bytes / bytes_per_task)
     if ray_tasks <= 0:
         ray_tasks = 1
         print("WARNING: Want 2GiB of RAM/ray-task.")
-        print("WARNING: Available memory (50% of total) is only {:.2f} GiB".format(usable_mem_bytes / gib))
+        print(
+            "WARNING: Available memory (50% of total) is only {:.2f} GiB".format(
+                usable_mem_bytes / gib
+            )
+        )
         print("WARNING: Will run on single core and hope to not use too much swap")
 
     if ray_tasks <= 1:
         ray_tasks = 2
-        print("WARNING: import_pdf_sort_benchmark requires 2 simultaneous ray tasks to not hang.")
-        print("WARNING: on low-memory containers, the logic to adjust the number of files to process")
-        print("WARNING: in a batch will clamp the number of files to 1 which will allow for importing")
+        print(
+            "WARNING: import_pdf_sort_benchmark requires 2 simultaneous ray tasks to not hang."
+        )
+        print(
+            "WARNING: on low-memory containers, the logic to adjust the number of files to process"
+        )
+        print(
+            "WARNING: in a batch will clamp the number of files to 1 which will allow for importing"
+        )
 
     return ray_tasks
 
@@ -185,7 +229,9 @@ def find_recursive(root, file_type):
     type_root = os.path.join(prefix, file_type)
     ret = []
     for path in Path(type_root).rglob("*"):
-        ret.append({"path": path, "suffix": path.relative_to(prefix), "type": file_type})
+        ret.append(
+            {"path": path, "suffix": path.relative_to(prefix), "type": file_type}
+        )
 
     return ret
 
@@ -217,7 +263,11 @@ def import_files(root, files):
         # textractor uses PIL.Image.open to identify images, and it fails with an
         # UnidentifiedImageError on a pdf file that doesn't end in .pdf.
         if i["type"] == "pdf" and not str(i["path"]).endswith(".pdf"):
-            print("ERROR: Unable to import", str(i["path"]), "-- pdf files must end in .pdf; fix the crawler")
+            print(
+                "ERROR: Unable to import",
+                str(i["path"]),
+                "-- pdf files must end in .pdf; fix the crawler",
+            )
             continue
 
         if i["type"] not in pending_paths:
@@ -272,11 +322,14 @@ def import_pdf(paths):
     ctx = sycamore_init()
     if enable_textract:
         table_extractor = TextractTableExtractor(
-            region_name="us-east-1", s3_upload_root=os.environ["SYCAMORE_TEXTRACT_PREFIX"]
+            region_name="us-east-1",
+            s3_upload_root=os.environ["SYCAMORE_TEXTRACT_PREFIX"],
         )
     else:
         table_extractor = None
-        print("WARNING: Textract disabled, results on sort benchmark website will be mediocre")
+        print(
+            "WARNING: Textract disabled, results on sort benchmark website will be mediocre"
+        )
 
     (
         # TODO: https://github.com/aryn-ai/quickstart/issues/2 - implement manifest generation
@@ -304,9 +357,15 @@ def import_pdf(paths):
         .spread_properties(["path", "title"])
         .explode()
         .embed(
-            embedder=SentenceTransformerEmbedder(batch_size=100, model_name="sentence-transformers/all-MiniLM-L6-v2")
+            embedder=SentenceTransformerEmbedder(
+                batch_size=100, model_name="sentence-transformers/all-MiniLM-L6-v2"
+            )
         )
-        .write.opensearch(os_client_args=get_os_client_args(), index_name=index, index_settings=get_index_settings())
+        .write.opensearch(
+            os_client_args=get_os_client_args(),
+            index_name=index,
+            index_settings=get_index_settings(),
+        )
     )
 
 
@@ -327,15 +386,23 @@ def import_html(paths):
         .partition(
             partitioner=HtmlPartitioner(
                 extract_tables=True,
-                text_chunker=TextOverlapChunker(chunk_token_count=4000, chunk_overlap_token_count=400),
+                text_chunker=TextOverlapChunker(
+                    chunk_token_count=4000, chunk_overlap_token_count=400
+                ),
             )
         )
         .spread_properties(["path", "title"])
         .explode()
         .embed(
-            embedder=SentenceTransformerEmbedder(batch_size=100, model_name="sentence-transformers/all-MiniLM-L6-v2")
+            embedder=SentenceTransformerEmbedder(
+                batch_size=100, model_name="sentence-transformers/all-MiniLM-L6-v2"
+            )
         )
-        .write.opensearch(os_client_args=get_os_client_args(), index_name=index, index_settings=get_index_settings())
+        .write.opensearch(
+            os_client_args=get_os_client_args(),
+            index_name=index,
+            index_settings=get_index_settings(),
+        )
     )
 
     # TODO: https://github.com/aryn-ai/sycamore/issues/160 - implement HTML import
@@ -345,20 +412,34 @@ def import_html(paths):
 def get_index_settings():
     return {
         "body": {
-            "settings": {"index.knn": True, "number_of_shards": 5, "number_of_replicas": 1},
+            "settings": {
+                "index.knn": True,
+                "number_of_shards": 5,
+                "number_of_replicas": 1,
+            },
             "mappings": {
                 "properties": {
                     "text": {"type": "text"},
                     "embedding": {
                         "dimension": 384,
-                        "method": {"engine": "nmslib", "space_type": "l2", "name": "hnsw", "parameters": {}},
+                        "method": {
+                            "engine": "nmslib",
+                            "space_type": "l2",
+                            "name": "hnsw",
+                            "parameters": {},
+                        },
                         "type": "knn_vector",
                     },
                     "title": {"type": "text"},
                     "searchable_text": {"type": "text"},
                     "title_embedding": {
                         "dimension": 384,
-                        "method": {"engine": "nmslib", "space_type": "l2", "name": "hnsw", "parameters": {}},
+                        "method": {
+                            "engine": "nmslib",
+                            "space_type": "l2",
+                            "name": "hnsw",
+                            "parameters": {},
+                        },
                         "type": "knn_vector",
                     },
                     "url": {"type": "text"},

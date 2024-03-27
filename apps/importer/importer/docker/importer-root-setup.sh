cb70bdbb7b5b65050144df64c42d29b2f056bbaa
Ben Sowell <ben@aryn.ai>
2024-03-27 12:56:35 -0700

    Add tesseract-ocr to container dependencies.
    Tesseract is used for OCR.

--- apps/importer/importer/docker/importer-root-setup.sh
+++ apps/importer/importer/docker/importer-root-setup.sh
@@ -5,4 +5,4 @@ set -x
 export DEBIAN_FRONTEND=noninteractive
 
 apt update
-apt -y install --no-install-recommends poppler-utils python3-poetry gcc python3-dev
+apt -y install --no-install-recommends poppler-utils tesseract-ocr python3-poetry gcc python3-dev

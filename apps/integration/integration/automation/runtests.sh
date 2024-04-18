49536db2c33d3a3a0cf64616d4300b3e5299d4ae
Henry Lindeman <hmlindeman@yahoo.com>
2024-04-18 09:31:43 -0700

    address pr comments. only build images for machines arch, and push after testing
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>

--- apps/integration/integration/automation/runtests.sh
+++ apps/integration/integration/automation/runtests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 TAG="$1"
-[[ -z "${TAG}" ]] && TAG="latest_rc"
+[[ -z "${TAG}" ]] && TAG="integration_tests"
 
-NOW="$(date +"%Y-%m-%d_%H_%M")"
+NOW="$(date +"%Y-%m-%d_%H_%M_%S")"
 ARCH="amd64"
 [[ "$(uname -m)" = "arm64" ]] && ARCH="arm64"
 
@@ -15,57 +15,56 @@ PYTEST_LOGFILE="${RUNDIR}/pytest.log"
 QUERY_LOGFILE="${RUNDIR}/test_queries.log"
 
 main() {
-  if [[ ! -d ".git" ]]; then
-    echo "Error: please run this script from sycamore root!" >&2
-    exit 1
-  fi
+  [[ ! -d ".git" ]] && die "Please run this script from sycamore root!"
   mkdir -p "${RUNDIR}"
   echo "Building/testing tag ${TAG}" >&2
   echo "Get the newest git commits" >&2
-  checkout_main_if_new
-  local should_run=$?
-  if [[ $should_run ]]; then
+  if [[ $(checkout_main_if_new) ]]; then
     echo "Changes detected. Running Tests" >&2
-    poetry install > "${POETRY_LOGFILE}" 2>&1
-    build_containers > "${DOCKER_LOGFILE}" 2>&1
-    runtests > "${PYTEST_LOGFILE}" 2>&1
+    poetry install > "${POETRY_LOGFILE}" 2>&1 \
+    && build_images > "${DOCKER_LOGFILE}" 2>&1 \
+    && runtests > "${PYTEST_LOGFILE}" 2>&1
     local passed_tests=$?
-    handle_outputs passed_tests
+    push_images >> "${DOCKER_LOGFILE}" 2>&1
+    handle_outputs $passed_tests
   else
     echo "No changes detected. Skipping integration tests" >&2
   fi
 }
 
 error() {
-  echo "ERROR: $@"
-  exit 1
+  echo "ERROR: $@" >&2
 }
 
 checkout_main_if_new() {
   old_sha="$(git rev-parse HEAD)"
-  git fetch origin main > "${GIT_LOGFILE}"
+  git fetch origin main > "${GIT_LOGFILE}" 2>&1
   new_sha="$(git rev-parse FETCH_HEAD)"
   if [[ "${old_sha}" != "${new_sha}" ]]; then
-    git pull --rebase origin main >> "${GIT_LOGFILE}"
+    [[ $(git status | grep -c 'nothing to commit, working tree clean') = 1 ]] \
+      || { echo "Working tree not clean" > "${GIT_LOGFILE}" && return 1; }
+    git pull --rebase origin main >> "${GIT_LOGFILE}" 2>&1
     return 0
   else
     return 1
   fi
 }
 
-build_containers() {
-  echo "Yep, definitely building containers. That's what this function does" >&2
-  docker-build-hub apps/crawler/crawler/http/Dockerfile
-  docker-build-hub apps/crawler/crawler/s3/Dockerfile
-  docker-build-hub apps/importer/Dockerfile.buildx
-  docker-build-hub apps/opensearch/Dockerfile
-  docker-build-hub apps/jupyter/Dockerfile.buildx --build-arg=TAG="${TAG}"
-  docker-build-hub apps/demo-ui/Dockerfile.buildx
-  docker-build-hub apps/remote-processor-service/Dockerfile.buildx
+build_images() {
+  echo "Building all images" >&2
+  docker-build-hub apps/crawler/crawler/http/Dockerfile \
+  && docker-build-hub apps/crawler/crawler/s3/Dockerfile \
+  && docker-build-hub apps/importer/Dockerfile.buildx \
+  && docker-build-hub apps/opensearch/Dockerfile \
+  && docker-build-hub apps/jupyter/Dockerfile.buildx --build-arg=TAG="${TAG}" \
+  && docker-build-hub apps/demo-ui/Dockerfile.buildx \
+  && docker-build-hub apps/remote-processor-service/Dockerfile.buildx \
+  && return 0
+  return 1
 }
 
 handle_outputs() {
-  echo "Yep, definitely handling test outputs. That's what this function does" >&2
+  echo "Handling test outputs" >&2
   local passed_tests="$1"
   mv test-output.log "${QUERY_LOGFILE}"
   [[ ${passed_tests} = 0 ]] && touch "${RUNDIR}/passed"
@@ -73,31 +72,56 @@ handle_outputs() {
   aws s3 cp -r "${RUNDIR}" "s3://sycamore-ci/${ARCH}"
 }
 
+push_images() {
+  echo "Pushing tested images to dockerhub" >&2
+  docker-push-hub apps/crawler/crawler/http/Dockerfile \
+  && docker-push-hub apps/crawler/crawler/s3/Dockerfile \
+  && docker-push-hub apps/importer/Dockerfile.buildx \
+  && docker-push-hub apps/opensearch/Dockerfile \
+  && docker-push-hub apps/jupyter/Dockerfile.buildx \
+  && docker-push-hub apps/demo-ui/Dockerfile.buildx \
+  && docker-push-hub apps/remote-processor-service/Dockerfile.buildx \
+  && return 0
+  return 1
+}
+
 runtests() {
   docker system prune -f --volumes
   docker compose up reset
   poetry run pytest apps/integration/ -p integration.conftest --noconftest --docker-tag "${TAG}"
-  # this is a complicated command, so: ^                        ^            ^ test against containers tagged latest_rc
-  #                                    |                     don't load conftest at pytest runtime; it's already loaded
-  #                                     load conftest with plugins, to capture the custom command line arg --docker-tag
+  # this is a complicated command, so:
+  # -p integration.conftest - load conftest with plugins, to capture the custom command line arg (--docker-tag)
+  # --noconftest            - don't load conftest at pytest runtime; it's already loaded
+  # --docker-tag            - specify tag of containers to test
   return $?
 }
 
 docker-build-hub() {
   local docker_file="$1"
-  [[ -n "${docker_file}" ]] || error "missing ${docker_file}"
+  [[ -n "${docker_file}" ]] || { error "missing ${docker_file}" && return 1;}
   local repo_name="$(_docker-repo-name "${docker_file}")"
-  [[ -n "${repo_name}" ]] || error "empty repo name"
+  [[ -n "${repo_name}" ]] || { error "empty repo name" && return 1;}
   shift
 
-  local platform=linux/amd64,linux/arm64
   echo
   echo "Building in sycamore and pushing to docker hub with repo name '${repo_name}'"
-  docker buildx build "$(_docker-build-args)" -t "${repo_name}:${TAG}" -f "${docker_file}" --platform ${platform} \
+  docker buildx build "$(_docker-build-args)" -t "${repo_name}:${TAG}" -f "${docker_file}" \
      --cache-to type=registry,ref="${repo_name}:build-cache",mode=max \
      --cache-from type=registry,ref="${repo_name}:build-cache" \
-     "$@" --push . || error "buildx failed"
-  echo "Successfully built in $dir using docker file $docker_file"
+     "$@" --load . || { error "buildx failed" && return 1;}
+  echo "Successfully built using docker file $docker_file"
+}
+
+docker-push-hub() {
+  local docker_file="$1"
+  [[ -n "${docker_file}" ]] || { error "missing ${docker_file}" && return 1;}
+  local repo_name="$(_docker-repo-name "${docker_file}")"
+  [[ -n "${repo_name}" ]] || { error "empty repo name" && return 1;}
+
+  echo
+  echo "Pushing image to docker hub for repo '${repo_name}"
+  docker push "${repo_name}:${TAG}" || { error "docker push failed" && return 1;}
+  echo "Successfully pushed image previously built from dockerfile ${docker_file}"
 }
 
 _docker-repo-name() {
@@ -112,7 +136,7 @@ _docker-repo-name() {
 }
 
 _docker-build-args() {
-  local branch="$(git status | head -1 | grep 'On branch ' | awk '{print $3}')"
+  local branch="$(git status | head -n1 | grep -i 'On branch ' | awk '{print $3}')"
   local rev="$(git rev-parse --short HEAD)"
   local date="$(git show -s --format=%ci HEAD | sed 's/ /_/g')"
   local diff=unknown

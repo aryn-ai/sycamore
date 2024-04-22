adf2a485eddde63374977d1017b3510825445f73
Ben Sowell <ben@aryn.ai>
2024-04-21 21:39:42 -0700

    Update data model for table structure recognition.
    This PR introduces a logical Table class that can be used to represent
    the data in a document table in a format independent way. It is based
    on representations used by TableTransformers and Textract, among
    others. This commit just adds the basic data structure and some format
    conversions and tests. Actual table understanding models will be added
    in a follow-up PR.
    
    This also includes some enhancements to the BoundingBox class so that
    it can be used with a variety of table models.

--- lib/sycamore/sycamore/data/__init__.py
+++ lib/sycamore/sycamore/data/__init__.py
@@ -1,5 +1,6 @@
 from sycamore.data.bbox import BoundingBox
 from sycamore.data.element import Element, TableElement
 from sycamore.data.document import Document, OpenSearchQuery, OpenSearchQueryResult
+from sycamore.data.table import Table
 
-__all__ = ["BoundingBox", "Document", "Element", "TableElement", "OpenSearchQuery", "OpenSearchQueryResult"]
+__all__ = ["BoundingBox", "Document", "Element", "TableElement", "OpenSearchQuery", "OpenSearchQueryResult", "Table"]
--- lib/sycamore/sycamore/data/bbox.py
+++ lib/sycamore/sycamore/data/bbox.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from typing import Tuple
+from collections.abc import Iterable
 
 
 class BoundingBox(ABC):
@@ -18,6 +18,19 @@ class BoundingBox(ABC):
         self.x2 = x2
         self.y2 = y2
 
+    @classmethod
+    def from_union(cls, boxes: Iterable["BoundingBox"]) -> "BoundingBox":
+        """Returns the BoundingBox formed by unioning the specified sequence of BoundingBoxes."""
+
+        bbox = EMPTY_BBOX.copy()
+        for new_box in iter(boxes):
+            bbox.update_union(new_box)
+
+        return bbox
+
+    def to_list(self) -> list[float]:
+        return [self.x1, self.y1, self.x2, self.y2]
+
     @property
     def height(self) -> float:
         return self.y2 - self.y1
@@ -27,20 +40,94 @@ class BoundingBox(ABC):
         return self.x2 - self.x1
 
     @property
-    def coordinates(self) -> Tuple[float, float, float, float]:
+    def coordinates(self) -> tuple[float, float, float, float]:
         return self.x1, self.y1, self.x2, self.y2
+
+    def copy(self):
+        return BoundingBox(*self.coordinates)
 
     def iou(self, other: "BoundingBox") -> float:
         """
         Intersection over union, we usually use this for detecting if two bbox cover the same entity.
         """
+        i = self.intersect(other).area
+
+        # Area of the union is sum of areas minus area of the intersection.
+        u = (self.area + other.area) - i
+
+        return i / u
+
+    def iob(self, other: "BoundingBox") -> float:
+        """Intersection over bounding box. This is another metric used for comparing overlap."""
+
+        if self.area > 0:
+            return self.intersect(other).area / self.area
+        return 0
+
+    def contains(self, other: "BoundingBox") -> bool:
+        return self.x1 <= other.x1 and self.x2 >= other.x2 and self.y1 <= other.y1 and self.y2 >= other.y2
+
+    def intersect(self, other: "BoundingBox") -> "BoundingBox":
         x1 = max(self.x1, other.x1)
         x2 = min(self.x2, other.x2)
         y1 = max(self.y1, other.y1)
         y2 = min(self.y2, other.y2)
-        i = 0 if x1 > x2 or y1 > y2 else (x2 - x1) * (y2 - y1)
-        u = (self.y2 - self.y1) * (self.x2 - self.x1) + (other.y2 - other.y1) * (other.x2 - other.x1) - i
-        return i / u
 
-    def contains(self, other: "BoundingBox") -> bool:
-        return self.x1 <= other.x1 and self.x2 >= other.x2 and self.y1 <= other.y1 and self.y2 >= other.y2
+        if x1 > x2 or y1 > y2:
+            return EMPTY_BBOX
+
+        return BoundingBox(x1, y1, x2, y2)
+
+    def translate(self, x, y) -> "BoundingBox":
+        """Translates (moves) this BoundingBox by the specified x and y values.
+
+        x and y may be negative.
+        """
+        # TODO: Do we want to allow the coordinates to be negative or truncate in the case that one or
+        # more of the coordinates goes < 0?
+        return BoundingBox(self.x1 + x, self.y1 + y, self.x2 + x, self.y2 + y)
+
+    def union(self, other: "BoundingBox") -> "BoundingBox":
+        return self.copy().update_union(other)
+
+    def update_union(self, other: "BoundingBox") -> "BoundingBox":
+        """Updates this BoundingBox in place to include the specified BoundingBox.
+
+        Note that A union B == B union A == A if B is empty,
+        regardless of the precise x and y values in B.
+        """
+
+        if self.is_empty():
+            self.x1, self.y1, self.x2, self.y2 = other.coordinates
+        elif other.is_empty():
+            pass
+        else:
+            self.x1 = min(self.x1, other.x1)
+            self.x2 = max(self.x2, other.x2)
+            self.y1 = min(self.y1, other.y1)
+            self.y2 = max(self.y2, other.y2)
+        return self
+
+    @property
+    def area(self) -> float:
+        return self.width * self.height
+
+    def to_relative(self, width, height) -> "BoundingBox":
+        """Converts this bounding box to be relative to the corresponding width and height."""
+
+        if width <= 0 or height <= 0:
+            raise ValueError(f"width and height must be > 0. Got ({width}, {height})")
+
+        return BoundingBox(self.x1 / width, self.y1 / height, self.x2 / width, self.y2 / height)
+
+    def to_absolute(self, width, height) -> "BoundingBox":
+        return BoundingBox(self.x1 * width, self.y1 * height, self.x2 * width, self.y2 * height)
+
+    def is_empty(self):
+        return self.x1 >= self.x2 and self.y1 >= self.y2
+
+    def __repr__(self):
+        return f"BoundingBox({self.x1}, {self.y1}, {self.x2}, {self.y2})"
+
+
+EMPTY_BBOX = BoundingBox(0, 0, 0, 0)

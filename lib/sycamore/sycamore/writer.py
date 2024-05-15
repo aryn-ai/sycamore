4dff961a253a7aa397e5e9405d0342919c48558d
Henry Lindeman <hmlindeman@yahoo.com>
2024-05-15 15:30:30 -0700

    add docstring for docset.write.weaviate
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>

--- lib/sycamore/sycamore/writer.py
+++ lib/sycamore/sycamore/writer.py
@@ -81,6 +81,88 @@ class DocSetWriter:
     def weaviate(
         self, *, wv_client_args: dict, collection_name: str, collection_config: Optional[dict] = None, **resource_args
     ) -> None:
+        """Writes the content of the DocSet into the specified Weaviate collection.
+
+        Args:
+            wv_client_args: Keyword parameters that are passed to the weaviate client constructor.
+                See more information at https://weaviate.io/developers/weaviate/client-libraries/python#python-client-v4-explicit-connection
+            collection_name: The name of the Weaviate collection into which to load this DocSet.
+            collection_config: Keyword parameters that are passed to the weaviate client's `collections.create()` method.
+                If not provided, Weaviate will Auto-Schematize the incoming records, which may lead to inconsistencies or failures.
+                See more information at https://weaviate.io/developers/weaviate/manage-data/collections#create-a-collection-and-define-properties
+
+        Example:
+            The following code shows how to read a pdf dataset into a ``DocSet`` and write it out to a
+            local Weaviate collection called `DemoCollection`.
+
+            .. code-block:: python
+
+
+                collection = "DemoCollection"
+                wv_client_args = {
+                    "connection_params": ConnectionParams.from_params(
+                        http_host="localhost",
+                        http_port=8080,
+                        http_secure=False,
+                        grpc_host="localhost",
+                        grpc_port=50051,
+                        grpc_secure=False,
+                    )
+                }
+
+                # Weaviate will assume empty arrays are empty arrays of text, so it
+                # will throw errors when you try to make an array of non-text in a
+                # field that some records have empty. => We specify them here.
+                collection_config_params = {
+                    "name": collection,
+                    "description": "A collection to demo data-prep with sycamore",
+                    "properties": [
+                        Property(
+                            name="properties",
+                            data_type=DataType.OBJECT,
+                            nested_properties=[
+                                Property(
+                                    name="links",
+                                    data_type=DataType.OBJECT_ARRAY,
+                                    nested_properties=[
+                                        Property(name="text", data_type=DataType.TEXT),
+                                        Property(name="url", data_type=DataType.TEXT),
+                                        Property(name="start_index", data_type=DataType.NUMBER),
+                                    ],
+                                ),
+                            ],
+                        ),
+                        Property(name="bbox", data_type=DataType.NUMBER_ARRAY),
+                        Property(name="shingles", data_type=DataType.INT_ARRAY),
+                    ],
+                    "vectorizer_config": [Configure.NamedVectors.text2vec_transformers(name="embedding")],
+                    "references": [ReferenceProperty(name="parent", target_collection=collection)],
+                }
+
+                model_name = "sentence-transformers/all-MiniLM-L6-v2"
+                davinci_llm = OpenAI(OpenAIModels.GPT_3_5_TURBO_INSTRUCT.value)
+                tokenizer = HuggingFaceTokenizer(model_name)
+
+                ctx = sycamore.init()
+
+                ds = ctx.read.binary(paths, binary_format="pdf")
+                    .partition(partitioner=UnstructuredPdfPartitioner())
+                    .regex_replace(COALESCE_WHITESPACE)
+                    .extract_entity(entity_extractor=OpenAIEntityExtractor("title", llm=davinci_llm, prompt_template=title_template))
+                    .mark_bbox_preset(tokenizer=tokenizer)
+                    .merge(merger=MarkedMerger())
+                    .spread_properties(["path", "title"])
+                    .split_elements(tokenizer=tokenizer, max_tokens=512)
+                    .explode()
+                    .embed(embedder=SentenceTransformerEmbedder(model_name=model_name, batch_size=100))
+                    .sketch(window=17)
+
+                ds.write.weaviate(
+                    wv_client_args=wv_client_args,
+                    collection_name=collection,
+                    collection_config=collection_config_params
+                )
+        """
         from sycamore.writers import WeaviateWriter
 
         wv = WeaviateWriter(self.plan, collection_name, wv_client_args, collection_config, **resource_args)

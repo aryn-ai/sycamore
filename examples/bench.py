14a20c317a21fd8e85a58cc7e5c664c8f1914dae
Alex Meyer <144723289+alexaryn@users.noreply.github.com>
2024-05-31 14:18:40 -0700

    Initial checkin of speed performance benchmarking script. (#433)
    * Initial checking on speed performance benchmarking script.
    
    * Explicit setting of ActorPool size.
    
    * Address comments.  Switch to gte-small.
    
    * lint

---
+++ examples/bench.py
@@ -0,0 +1,54 @@
+# This is the current base speed performance benchmarking script.
+# For actual timing runs, it may need to be modified to set the
+# numbers of CPUs, GPUs, batches, devices, etc.  Other Sycamore
+# code may also need to be changed to adjust actor pools.  The
+# script should be run similar to this:
+#
+# TIMETRACE=/tmp/tt poetry run python examples/bench.py
+
+import pyarrow.fs
+
+from ray.data import ActorPoolStrategy
+
+import sycamore
+from sycamore.functions.tokenizer import HuggingFaceTokenizer
+from sycamore.llms import OpenAIModels, OpenAI
+from sycamore.transforms import COALESCE_WHITESPACE
+from sycamore.transforms.merge_elements import MarkedMerger
+from sycamore.transforms.partition import SycamorePartitioner
+from sycamore.transforms.extract_entity import OpenAIEntityExtractor
+from sycamore.transforms.embed import SentenceTransformerEmbedder
+
+from simple_config import idx_settings, osrch_args, title_template
+
+paths = ["s3://aryn-public/ntsb/"]
+index = "demoindex0"
+fsys = pyarrow.fs.S3FileSystem(region="us-east-1", anonymous=True)
+
+davinci_llm = OpenAI(OpenAIModels.GPT_3_5_TURBO_INSTRUCT.value)
+tokenizer = HuggingFaceTokenizer("thenlper/gte-small")
+
+ctx = sycamore.init()
+
+ds = (
+    ctx.read.binary(paths, binary_format="pdf", filesystem=fsys)
+    .partition(
+        partitioner=SycamorePartitioner(extract_table_structure=True, extract_images=True),
+        compute=ActorPoolStrategy(size=1),
+    )
+    .regex_replace(COALESCE_WHITESPACE)
+    .extract_entity(entity_extractor=OpenAIEntityExtractor("title", llm=davinci_llm, prompt_template=title_template))
+    .mark_bbox_preset(tokenizer=tokenizer)
+    .merge(merger=MarkedMerger())
+    .spread_properties(["path", "title"])
+    .split_elements(tokenizer=tokenizer, max_tokens=512)
+    .explode()
+    .sketch()
+    .embed(embedder=SentenceTransformerEmbedder(model_name="thenlper/gte-small", batch_size=100))
+)
+
+ds.write.opensearch(
+    os_client_args=osrch_args,
+    index_name=index,
+    index_settings=idx_settings,
+)

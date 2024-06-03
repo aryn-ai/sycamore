2522c1e1673eab3b6609613d6b75c00b8039f5cb
sohamkasar19 <sohamkasar19@gmail.com>
2024-06-03 11:47:54 -0700

    rename pdfviewer file

--- apps/demo-ui/ui/src/App.tsx
+++ apps/demo-ui/ui/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PdfViewer from "./pages/PDFViewer/PdfViewer";
+import PdfViewer from "./pages/PDFViewer";
 import HomePage from "./pages/HomePage";
 
 function App() {

59f9d07cf3272ce1fcd47629ef2327a4c10efcfa
Eric Anderson <eric@aryn.ai>
2024-06-07 15:53:50 -0700

    lots of test failures

--- lib/sycamore/sycamore/tests/test_github_checking.py
+++ lib/sycamore/sycamore/tests/test_github_checking.py
@@ -1,2 +1,16 @@
 """A totally benign test, definitely won't cause issues.
 This change definitely not by evil eric."""
+
+def typechecking_fail() -> None:
+    a: int = "I can be an int if I want to be"
+
+# call errors
+github.should_not_let_me_commit_this()
+
+# reformatting errors
+1 +        2 +      3
+
+# and some trailing blank lines for bonus
+  
+
+            

345cb5c1516b528cacdb847ff24f1d00356f98e8
Ben Sowell <ben@aryn.ai>
2024-06-17 20:27:00 -0700

    Fix show_pages to work wtih MetadataDocuments. (#471)

--- lib/sycamore/sycamore/utils/pdf_utils.py
+++ lib/sycamore/sycamore/utils/pdf_utils.py
@@ -1,22 +1,18 @@
 from io import BytesIO
 from PIL import Image
 
-from sycamore.data import Document
-from sycamore import DocSet, Execution
+from sycamore import DocSet
 from sycamore.functions.document import DrawBoxes, split_and_convert_to_image
 from sycamore.utils.image_utils import show_images
 
 
 def show_pages(docset: DocSet, limit: int = 2):
-    new_docset = (
+    documents = (
         docset.flat_map(split_and_convert_to_image)
         .limit(limit)
         .map_batch(DrawBoxes(), f_constructor_kwargs={"draw_table_cells": True})
+        .take(limit)
     )
-
-    execution = Execution(new_docset.context, new_docset.plan)
-    dataset = execution.execute(new_docset.plan)
-    documents = [Document.from_row(row) for row in dataset.take(limit)]
     images = [
         Image.open(BytesIO(doc.binary_representation)) for doc in documents if doc.binary_representation is not None
     ]

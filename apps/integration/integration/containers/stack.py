ec2f68ccbd614d0936e7b7d70ae0059a9fe587b2
Eric Anderson <eric@aryn.ai>
2024-06-21 15:54:50 -0700

    Get integration tests working again. (#484)
    Important fixes:
      * Add a ludicrous amount of additional logging so it's rare that something isn't logged after a
        short wait.  Logs go to error because I was having some issues with info logging not showing
        up.  That may be fixed now, but unclear.
      * rm -rf /app/.scrapy/imported /app/.scrapy/downloads instead of *; * isn't being expanded
      * Fix imported files via find in the filesystem instead of parsing output.
        The ray output and importer output are sometimes interleaved on the same line
      * Verify that the scraper downloaded something before continuing
      * Tolerate missing indices on deletion
      * Enable optionally waiting before aborting the test so state is preserved for inspection
      * Add more debugging on the opensearch side, although bugs appeared to all be test-side.
      * Update automation to take advantage of debugging

--- apps/integration/integration/containers/stack.py
+++ apps/integration/integration/containers/stack.py
@@ -2,6 +2,10 @@ from testcontainers.compose import DockerCompose
 import pytest
 from integration import SYCAMORE_ROOT
 from typing import List, Union
+import logging
+import subprocess
+import os
+import time
 
 
 def docker_compose(services: Union[List[str], None] = None):
@@ -35,6 +39,26 @@ def stack(tag):
     base_compose = docker_compose()
     base_compose.pull = True
     set_version_tag(tag)
+    logging.error(f"Setting up compose with tag {tag} .. this may take a long time")
     with base_compose:
+        logging.error("Done")
         yield base_compose
+
+        logs = open("docker-logs.txt", "w")
+        subprocess.call(["docker", "compose", "logs"], stdout=logs)
+        logging.error("Wrote logs to docker-logs.txt")
+        maybe_wait_to_abort()
+        logging.error("Shutting down docker, this can take a little bit")
     set_version_tag("stable")
+
+
+def maybe_wait_to_abort():
+    if os.environ.get("NOEXIT", "") == "":
+        logging.error("You can set the environment variable NOEXIT to force a pause at this point")
+        return
+
+    while True:
+        logging.error("Waiting for /tmp/abort to exist")
+        if os.path.isfile("/tmp/abort"):
+            break
+        time.sleep(1)

016d92e171c980c78cb7fc3aeef438f6f093f61d
Henry Lindeman <hmlindeman@yahoo.com>
2024-06-25 14:48:48 -0700

    add connectors/opensearch/__init__ -- forgot in last one
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>

---
+++ lib/sycamore/sycamore/connectors/opensearch/__init__.py
@@ -0,0 +1,13 @@
+from sycamore.connectors.opensearch.opensearch_scan import OpenSearchScan
+from sycamore.connectors.opensearch.opensearch_writer import (
+    OpenSearchWriter,
+    OpenSearchClientParams,
+    OpenSearchTargetParams,
+)
+
+__all__ = [
+    "OpenSearchScan",
+    "OpenSearchWriter",
+    "OpenSearchClientParams",
+    "OpenSearchTargetParams",
+]

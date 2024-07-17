fc073a67ea57e6c7fedf03e6b9b45066025a8bae
Mark Lindblad <mnl@berkeley.edu>
2024-07-17 14:20:29 -0700

    Remove unused import

--- lib/sycamore/sycamore/transforms/detr_partitioner.py
+++ lib/sycamore/sycamore/transforms/detr_partitioner.py
@@ -4,7 +4,6 @@ import os
 import tempfile
 import tracemalloc
 from abc import ABC, abstractmethod
-from collections.abc import Mapping
 from concurrent.futures import ProcessPoolExecutor
 from io import BytesIO, IOBase
 from typing import cast, Any, BinaryIO, List, Tuple, Union

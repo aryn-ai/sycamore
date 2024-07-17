63cbbba8d578549c47d7589f48f58cce11f564d6
Ritam Saha <ritam@aryn.ai>
2024-07-17 01:31:28 +0000

    added documentation to GraphMetaData

--- lib/sycamore/sycamore/transforms/extract_graph.py
+++ lib/sycamore/sycamore/transforms/extract_graph.py
@@ -11,11 +11,13 @@ class GraphData(ABC):
 
 class GraphMetadata(GraphData):
     """
-    Turn document metadata into nodes to put into a graph
+    Args:
+        nodeKey: Key used to access document metadata in the document['properties] dictionary
+        nodeLabel: The label used in neo4j the node of a piece of metadata
+        relLabel: The label used in neo4j for the relationship between the document and a piece of metadata
     """
     def __init__(self, nodeKey: str, nodeLabel: str, relLabel: str):
         self.nodeKey = nodeKey
         self.nodeLabel = nodeLabel
         self.relLabel = relLabel
-        self.labels = None
 

20f73d4807c59b15eaf502cc201647d66d36fe36
Tanvi Ranade <tanviranade@gmail.com>
2024-07-19 09:57:37 -0700

    make mypy happy

--- lib/sycamore/sycamore/tests/unit/transforms/test_sort.py
+++ lib/sycamore/sycamore/tests/unit/transforms/test_sort.py
@@ -14,15 +14,14 @@ class TestSort:
             # text_representation is random 6 letter strings
             Document(
                 text_representation=f"{''.join(random.choices(string.ascii_letters, k=6))}",
-                doc_id={"even": i if i % 2 == 0 else None},
-                properties={"document_number": random.randint(1, 10000)},
+                properties={"document_number": random.randint(1, 10000), "even": i if i % 2 == 0 else None},
             )
             for i in range(10)
         ]
 
         for doc in doc_list:
-            if doc.doc_id["even"] is None:
-                doc.doc_id = {}
+            if doc.properties["even"] is None:
+                doc.properties.pop("even")
         return doc_list
 
     @pytest.fixture()
@@ -35,7 +34,7 @@ class TestSort:
         doc_list = sorted_docset.take_all()
 
         for i in range(1, len(doc_list)):
-            assert doc_list[i].text_representation <= doc_list[i - 1].text_representation
+            assert str(doc_list[i].text_representation) <= str(doc_list[i - 1].text_representation)
 
     def test_sort_ascending(self, docset: DocSet):
         sorted_docset = docset.sort(False, "properties.document_number")
@@ -45,8 +44,8 @@ class TestSort:
             assert doc_list[i].properties["document_number"] >= doc_list[i - 1].properties["document_number"]
 
     def test_default_value(self, docset: DocSet):
-        sorted_docset = docset.sort(False, "doc_id.even", 0)
+        sorted_docset = docset.sort(False, "properties.even", 0)
         doc_list = sorted_docset.take_all()
 
         for i in range(1, len(doc_list)):
-            assert doc_list[i].doc_id.get("even", 0) >= doc_list[i - 1].doc_id.get("even", 0)
+            assert doc_list[i].properties.get("even", 0) >= doc_list[i - 1].properties.get("even", 0)

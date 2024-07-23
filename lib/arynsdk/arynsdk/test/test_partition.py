48899eeef5f4d0699abf28653f83bf3a41432103
Henry Lindeman <hmlindeman@yahoo.com>
2024-07-23 10:19:26 -0700

    improve partition_file testing
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>

--- lib/arynsdk/arynsdk/test/test_partition.py
+++ lib/arynsdk/arynsdk/test/test_partition.py
@@ -1,64 +1,103 @@
-import os
-from pprint import pprint
+import pytest
+import json
+from pathlib import Path
 
-from arynsdk.partition import partition_file, tables_to_pandas
-from arynsdk.config import ArynConfig
+from arynsdk.partition import partition_file
+from requests.exceptions import HTTPError
 
-# Replace with your token
-aryn_token = os.environ["ARYN_API_KEY"]
-
-# Replace with test files
-simple_test_file = "/Users/hmlin/Aryn/sycamore/.data/tables/3m_table.pdf"
+RESOURCE_DIR = Path(__file__).parent / "resources"
 
 
-def test_partition_file():
+@pytest.mark.parametrize(
+    "pdf, kwargs, response",
+    [
+        (
+            RESOURCE_DIR / "pdfs" / "3m_table.pdf",
+            {},
+            RESOURCE_DIR / "json" / "3m_output.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "3m_table.pdf",
+            {"use_ocr": True},
+            RESOURCE_DIR / "json" / "3m_output_ocr.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "3m_table.pdf",
+            {"use_ocr": True, "extract_table_structure": True},
+            RESOURCE_DIR / "json" / "3m_output_ocr_table.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "3m_table.pdf",
+            {"extract_table_structure": True},
+            RESOURCE_DIR / "json" / "3m_output_table.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "SPsort.pdf",
+            {},
+            RESOURCE_DIR / "json" / "SPsort_output.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "SPsort.pdf",
+            {"extract_images": True},
+            RESOURCE_DIR / "json" / "SPsort_output_images.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "SPsort.pdf",
+            {"extract_images": True, "selected_pages": [0]},
+            RESOURCE_DIR / "json" / "SPsort_output_images_page0.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "SPsort.pdf",
+            {"extract_images": True, "selected_pages": [1]},
+            RESOURCE_DIR / "json" / "SPsort_output_images_page1.json",
+        ),
+    ],
+)
+def test_partition(pdf, kwargs, response, mocker):
+    with open(response, "r") as f:
+        response_data = json.load(f)
+    resp_object = mocker.Mock()
+    resp_object.status_code = 200
+    resp_object.json.return_value = response_data
 
-    f = open(simple_test_file, "rb")
-    my_resp = partition_file(f, aryn_api_key=aryn_token)
-    print("----------")
-    pprint(my_resp)
-    f.close()
+    mocker.patch("requests.post").return_value = resp_object
 
-    f = open(simple_test_file, "rb")
-    my_resp = partition_file(f, aryn_config=ArynConfig(aryn_api_key=aryn_token), use_ocr=True)
-    print("----------")
-    pprint(my_resp)
-    f.close()
-
-    f = open(simple_test_file, "rb")
-    my_resp = partition_file(f, threshold=0.35)
-    print("----------")
-    pprint(my_resp)
-
-    f.seek(0)
-    try:
-        my_resp = partition_file(f, aryn_token, my_thres=0.4)
-        print("----------")
-        pprint(my_resp)
-    except TypeError as k:
-        print(f"Caught incorrect param: {k}")
-
-    f.seek(0)
-    my_resp = partition_file(f, extract_table_structure=True, extract_images=True)
-    print("----------")
-    pprint(my_resp)
+    with open(pdf, "rb") as f:
+        new_response = partition_file(f, **kwargs)
+    assert new_response == response_data
 
 
-def test_table():
-    with open(simple_test_file, "rb") as f:
-        response = partition_file(f, extract_table_structure=True, use_ocr=True)
-    elts_and_tables = tables_to_pandas(response)
-    for table, df in filter(lambda e: e[0]["type"] == "table", elts_and_tables):
-        print(df)
+@pytest.mark.parametrize(
+    "pdf, kwargs, response",
+    [
+        (
+            RESOURCE_DIR / "pdfs" / "3m_table.pdf",
+            {},
+            RESOURCE_DIR / "json" / "3m_output.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "3m_table.pdf",
+            {"use_ocr": True, "extract_table_structure": True},
+            RESOURCE_DIR / "json" / "3m_output_ocr_table.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "SPsort.pdf",
+            {"extract_images": True, "selected_pages": [1]},
+            RESOURCE_DIR / "json" / "SPsort_output_images_page1.json",
+        ),
+    ],
+)
+def test_partition_it(pdf, kwargs, response):
+    with open(response, "r") as f:
+        response_data = json.load(f)
+    with open(pdf, "rb") as f:
+        new_response = partition_file(f, **kwargs)
+    assert response_data["elements"] == new_response["elements"]
 
 
-# def test_large_file():
-
-#     f = open(large_test_file, "rb")
-#     my_resp = partition_file(f, aryn_token, threshold=0.35)
-#     print("----------")
-#     pprint(my_resp)
-
-if __name__ == "__main__":
-    test_table()
-    test_partition_file()
+def test_partition_it_no_api_key():
+    with pytest.raises(HTTPError) as einfo:
+        with open(RESOURCE_DIR / "pdfs" / "3m_table.pdf", "rb") as f:
+            partition_file(f, aryn_api_key="")
+    assert einfo.value.response.status_code == 403
+    assert einfo.value.response.json().get("detail") == "Not authenticated"

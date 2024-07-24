c5c8016b0a057c52928be7a8396a0c93adbef6d7
Mark Lindblad <mnl@berkeley.edu>
2024-07-24 16:55:16 -0700

    Fix formatting

--- lib/sycamore/sycamore/transforms/detr_partitioner.py
+++ lib/sycamore/sycamore/transforms/detr_partitioner.py
@@ -40,6 +40,7 @@ from sycamore.utils.time_trace import LogTime, timetrace
 logger = logging.getLogger(__name__)
 _DETR_LOCK_FILE = "/tmp/Aryn-Detr.lock"
 
+
 def _batchify(iterable, n=1):
     length = len(iterable)
     for i in range(0, length, n):

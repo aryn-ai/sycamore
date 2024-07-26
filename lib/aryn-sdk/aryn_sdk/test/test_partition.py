e2adc908f47e5a4c015abe8ccd15bf363780894e
Mehul A. Shah <mashah@gmail.com>
2024-07-26 11:44:15 -0700

    Creating the Aryn SDK  (#560)
    * Python client for the Aryn Partitioning Service.
    
    Includes partition function that segments PDFs and returns a JSON.
    
    * Added the files in the wrong place. Removing.
    
    * Ran poetry black and mypy.
    
    * Unsure
    
    * tabular pandafication
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * misc pr fixes
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * move arynsdk to from apps to lib
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * turn arynsdk into a poetry project
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * depend arynsdk on poetry-lock
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * docstring and import from partition directly
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * move partition/test_client.py to test/test_partition.py
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * read api keys in various ways
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * add selected_pages arg
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * decompose tables_to_pandas from partition_file
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * forgot this was still commented
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * fix ruff
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * document 1-indexed pdf pages
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * improve partition_file testing
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * add test for pandafication
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * whoops forgot to add these
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * fill in readme
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * fix dependencies and change to aryn-sdk
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * fix imports
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * screw with workflows to make publishing possible I think
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * add ocr_tables and ocr_images flags
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * support streaming from partitioner
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * pr fixes
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * fix readme
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * simplify release to pure pypi upload on tag push
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * add draw_with_boxes
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * add image processing deps to sdk
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * Note for installing poppler
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * Fixed tests and partition_file.
    
     - Status logging for streaming results.
     - Use the correct warning function.
     - Added streaming test with pages of 0 offset.
    
    * Set source to aryn-sdk in partition_file
    
    * update readme and add table_elem_to_dataframe to __init__.py
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * add docs for table_elem_to_dataframe
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    ---------
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    Co-authored-by: Henry Lindeman <hmlindeman@yahoo.com>

---
+++ lib/aryn-sdk/aryn_sdk/test/test_partition.py
@@ -0,0 +1,139 @@
+from aryn_sdk.partition.partition import tables_to_pandas
+import pytest
+import json
+from pathlib import Path
+
+from aryn_sdk.partition import partition_file
+from requests.exceptions import HTTPError
+
+RESOURCE_DIR = Path(__file__).parent / "resources"
+
+
+# Unit tests
+@pytest.mark.parametrize(
+    "pdf, kwargs, response",
+    [
+        (
+            RESOURCE_DIR / "pdfs" / "3m_table.pdf",
+            {},
+            RESOURCE_DIR / "json" / "3m_output.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "3m_table.pdf",
+            {"use_ocr": True},
+            RESOURCE_DIR / "json" / "3m_output_ocr.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "3m_table.pdf",
+            {"use_ocr": True, "extract_table_structure": True},
+            RESOURCE_DIR / "json" / "3m_output_ocr_table.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "3m_table.pdf",
+            {"extract_table_structure": True},
+            RESOURCE_DIR / "json" / "3m_output_table.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "SPsort.pdf",
+            {},
+            RESOURCE_DIR / "json" / "SPsort_output.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "SPsort.pdf",
+            {"extract_images": True},
+            RESOURCE_DIR / "json" / "SPsort_output_images.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "SPsort.pdf",
+            {"extract_images": True, "selected_pages": [0]},
+            RESOURCE_DIR / "json" / "SPsort_output_images_page0.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "SPsort.pdf",
+            {"extract_images": True, "selected_pages": [1]},
+            RESOURCE_DIR / "json" / "SPsort_output_images_page1.json",
+        ),
+    ],
+)
+def test_partition(pdf, kwargs, response, mocker):
+
+    with open(response, "r") as f:
+        response_data = json.load(f)
+    resp_object = mocker.Mock()
+    resp_object.status_code = 200
+
+    # Mock the response from the file,
+    resp_object.iter_lines.return_value = json.dumps(response_data, indent=2).encode("UTF-8").split(sep=b"\n")
+
+    mocker.patch("requests.post").return_value = resp_object
+
+    with open(pdf, "rb") as f:
+        if kwargs.get("selected_pages") == [0]:
+            with pytest.raises(ValueError) as einfo:
+                new_response = partition_file(f, **kwargs)
+            assert "Invalid page number (0)" in str(einfo.value)
+        else:
+            new_response = partition_file(f, **kwargs)
+            assert new_response == response_data
+
+
+# Integration tests
+
+
+@pytest.mark.parametrize(
+    "pdf, kwargs, response",
+    [
+        (
+            RESOURCE_DIR / "pdfs" / "3m_table.pdf",
+            {},
+            RESOURCE_DIR / "json" / "3m_output.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "3m_table.pdf",
+            {"use_ocr": True, "extract_table_structure": True},
+            RESOURCE_DIR / "json" / "3m_output_ocr_table.json",
+        ),
+        (
+            RESOURCE_DIR / "pdfs" / "SPsort.pdf",
+            {"extract_images": True, "selected_pages": [1]},
+            RESOURCE_DIR / "json" / "SPsort_output_images_page1.json",
+        ),
+    ],
+)
+def test_partition_it(pdf, kwargs, response):
+
+    with open(response, "r") as f:
+        response_data = json.load(f)
+
+    with open(pdf, "rb") as f:
+        new_response = partition_file(f, **kwargs)
+
+    assert response_data["elements"] == new_response["elements"]
+
+
+def test_partition_it_zero_page():
+
+    with pytest.raises(ValueError) as einfo:
+        with open(RESOURCE_DIR / "pdfs" / "SPsort.pdf", "rb") as f:
+            partition_file(f, selected_pages=[0])
+
+    assert "Invalid page number (0)" in str(einfo.value)
+
+
+def test_partition_it_no_api_key():
+    with pytest.raises(HTTPError) as einfo:
+        with open(RESOURCE_DIR / "pdfs" / "3m_table.pdf", "rb") as f:
+            partition_file(f, aryn_api_key="")
+    assert einfo.value.response.status_code == 403
+    assert einfo.value.response.json().get("detail") == "Not authenticated"
+
+
+def test_data_to_pandas():
+    with open(RESOURCE_DIR / "json" / "3m_output_ocr_table.json", "r") as f:
+        data = json.load(f)
+    elts_and_dfs = tables_to_pandas(data)
+    assert len(elts_and_dfs) == 5
+    df = elts_and_dfs[0][1]
+    assert df is not None
+    assert df.columns.to_list() == ["(Millions)", "2018", "2017", "2016"]
+    assert df["2018"][13] == "134"

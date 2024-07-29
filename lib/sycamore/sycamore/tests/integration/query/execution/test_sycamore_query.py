d877f0b703c9c246de7cfcd3c26caa7505f835d4
Vinayak Thapliyal <vinayak@aryn.ai>
2024-07-29 11:56:34 -0700

    Add codegen versions of basic query integration tests (#595)
    * Add cogen versions of basic query integration tests

--- lib/sycamore/sycamore/tests/integration/query/execution/test_sycamore_query.py
+++ lib/sycamore/sycamore/tests/integration/query/execution/test_sycamore_query.py
@@ -1,20 +1,30 @@
+import contextlib
+import io
+
+import pytest
+
 from sycamore.query.client import SycamoreQueryClient
 
 
 class TestSycamoreQuery:
 
-    def test_simple(self, query_integration_test_index: str):
+    @pytest.mark.parametrize("codegen", [True, False])
+    def test_simple(self, query_integration_test_index: str, codegen: bool):
         """
         Simple test that ensures we can run a query end to end and get a text response.
         """
         client = SycamoreQueryClient()
         schema = client.get_opensearch_schema(query_integration_test_index)
         plan = client.generate_plan("How many incidents happened in california?", query_integration_test_index, schema)
-        query_id, result = client.run_plan(plan)
+        query_id, result = client.run_plan(plan, dry_run=codegen)
         assert isinstance(result, str)
         assert len(result) > 0
+        if codegen:
+            print(result)
+            exec(result)
 
-    def test_forked(self, query_integration_test_index: str):
+    @pytest.mark.parametrize("codegen", [True, False])
+    def test_forked(self, query_integration_test_index: str, codegen: bool):
         """
         Test that has a fork in the DAG, ensures we can support multiple execution paths.
         """
@@ -23,7 +33,13 @@ class TestSycamoreQuery:
         plan = client.generate_plan(
             "What percentage of all incidents happened in california", query_integration_test_index, schema
         )
-        query_id, result = client.run_plan(plan)
+        query_id, result = client.run_plan(plan, dry_run=codegen)
         assert isinstance(result, str)
         assert len(result) > 0
+        if codegen:
+            print(result)
+            output = io.StringIO()
+            with contextlib.redirect_stdout(output):
+                exec(result)
+            result = output.getvalue()
         assert "%" in result
--- lib/sycamore/sycamore/tests/integration/query/test_planner.py
+++ lib/sycamore/sycamore/tests/integration/query/test_planner.py
@@ -2,7 +2,7 @@ from typing import List
 
 from opensearchpy import OpenSearch
 
-from integration.query.conftest import OS_CLIENT_ARGS, OS_CONFIG
+from sycamore.tests.integration.query.conftest import OS_CLIENT_ARGS, OS_CONFIG
 from sycamore.query.logical_plan import Node
 from sycamore.query.planner import LlmPlanner
 
@@ -12,7 +12,7 @@ def test_simple_openai_planner(query_integration_test_index: str):
     Simple test ensuring nodes are being creating and dependencies are being set.
     Using a simple query here for consistent query plans.
     """
-    os_client = OpenSearch(**OS_CLIENT_ARGS)
+    os_client = OpenSearch(OS_CLIENT_ARGS)
 
     schema = {"location": "string", "airplaneType": "string"}
     planner = LlmPlanner(query_integration_test_index, data_schema=schema, os_config=OS_CONFIG, os_client=os_client)

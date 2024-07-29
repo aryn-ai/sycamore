9585ece53d678a6c2e2807992740d54c89a492bb
Tanvi Ranade <tanviranade@gmail.com>
2024-07-29 14:56:55 -0700

    reset llm query

--- lib/sycamore/sycamore/transforms/llm_query.py
+++ lib/sycamore/sycamore/transforms/llm_query.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Any, Union
+from typing import Optional, Any, Union
 
 from sycamore.data import Element, Document
 from sycamore.plan_nodes import NonCPUUser, NonGPUUser, Node
@@ -44,8 +44,6 @@ class LLMTextQueryAgent:
         number_of_elements: Optional[int] = None,
         llm_kwargs: dict = {},
         per_element: bool = True,
-        messages: Optional[List[dict]] = None,
-        field: str = None
     ):
         self._llm = llm
         self._prompt = prompt
@@ -54,8 +52,6 @@ class LLMTextQueryAgent:
         self._per_element = per_element
         self._format_kwargs = format_kwargs
         self._number_of_elements = number_of_elements
-        self._messages = messages
-        self._field = field
 
     def execute_query(self, document: Document) -> Document:
         if self._per_element:
@@ -77,32 +73,20 @@ class LLMTextQueryAgent:
         else:
             if document.text_representation:
                 document = self._query_text_object(document)
-            else:
-                document.properties[self._output_property] = 'None'
         return document
 
     @timetrace("LLMQueryText")
     def _query_text_object(self, object: Union[Document, Element]) -> Union[Document, Element]:
         if object.text_representation:
-            if self._messages:
-                value = object.field_to_value(self._field if self._field else "text_representation")
-                self._messages.append(
-                    {
-                        "role": "user",
-                        "content": f"{value}",
-                    }
+            if self._format_kwargs:
+                prompt = (
+                    SandboxedEnvironment()
+                    .from_string(source=self._prompt, globals=self._format_kwargs)
+                    .render(doc=object)
                 )
-                prompt_kwargs = {"messages": self._messages}
             else:
-                if self._format_kwargs:
-                    prompt = (
-                        SandboxedEnvironment()
-                        .from_string(source=self._prompt, globals=self._format_kwargs)
-                        .render(doc=object)
-                    )
-                else:
-                    prompt = self._prompt + "\n" + object["text_representation"]
-                prompt_kwargs = {"prompt": prompt}
+                prompt = self._prompt + "\n" + object["text_representation"]
+            prompt_kwargs = {"prompt": prompt}
             llm_resp = self._llm.generate(prompt_kwargs=prompt_kwargs, llm_kwargs=self._llm_kwargs)
             object["properties"][self._output_property] = llm_resp
         return object

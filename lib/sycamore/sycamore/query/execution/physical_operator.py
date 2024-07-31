a4d2342d45fdc4d8603713afa3142cfacb8fe2c2
Matt Welsh <matt@aryn.ai>
2024-07-31 14:22:20 -0700

    Refactor LogicalOperators to use pydantic. (#610)
    * Use Helicone for CI runs.
    
    * WIP: Refactor LogicalOperators to use pydantic.
    
    * Working on this.
    
    * Working on this
    
    * Fix up a lot of stuff.
    
    * Linting
    
    * Working on this.
    
    * Fix tests.
    
    * Fix query.
    
    * Fix query UI.
    
    * Fix serialization of query plans.
    
    * Fix lint.

--- lib/sycamore/sycamore/query/execution/physical_operator.py
+++ lib/sycamore/sycamore/query/execution/physical_operator.py
@@ -1,13 +1,15 @@
 from abc import abstractmethod
 from typing import List, Any, Optional, Tuple, Dict
 
+from sycamore.query.logical_plan import Node
 from sycamore.query.operators.logical_operator import LogicalOperator
+from sycamore.query.operators.math import Math
 
 from sycamore.query.execution.operations import math_operation
 
 
-def get_var_name(logical_node: LogicalOperator):
-    return f"output_{logical_node.node_id}"
+def get_var_name(node: Node):
+    return f"output_{node.node_id}"
 
 
 def get_str_for_dict(value: Dict):
@@ -57,12 +59,12 @@ class MathOperator(PhysicalOperator):
     Perform basic math on 2 input variables.
     """
 
-    def __init__(self, logical_node: LogicalOperator, query_id: str, inputs: List[Any]) -> None:
+    def __init__(self, logical_node: Math, query_id: str, inputs: List[Any]) -> None:
         super().__init__(logical_node, query_id, inputs)
 
     def execute(self) -> Any:
-        assert self.logical_node.data is not None
-        operator = self.logical_node.data.get("type")
+        assert isinstance(self.logical_node, Math)
+        operator = self.logical_node.operation
         assert (
             self.inputs is not None
             and len(self.inputs) == 2
@@ -79,10 +81,9 @@ class MathOperator(PhysicalOperator):
         return result
 
     def script(self, input_var: Optional[str] = None, output_var: Optional[str] = None) -> Tuple[str, List[str]]:
+        assert isinstance(self.logical_node, Math)
         assert self.logical_node.dependencies is not None and len(self.logical_node.dependencies) == 2
-        assert self.logical_node.data is not None
-        operator = self.logical_node.data.get("type")
-        assert operator is not None and isinstance(operator, str)
+        operator = self.logical_node.operation
         result = f"""
 {output_var or get_var_name(self.logical_node)} = math_operation(
     val1={input_var or get_var_name(self.logical_node.dependencies[0])},

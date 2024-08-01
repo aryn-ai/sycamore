9ac0f2b3ef087f87887a6c2e2d04acb9e7b78ee9
Tanvi Ranade <tanviranade@gmail.com>
2024-07-31 18:24:10 -0700

    fix tests

--- lib/sycamore/sycamore/tests/unit/query/execution/test_sycamore_operator.py
+++ lib/sycamore/sycamore/tests/unit/query/execution/test_sycamore_operator.py
@@ -139,7 +139,7 @@ def test_count():
 
     return_value_count = 5
     doc_set.count.return_value = return_value_count
-    logical_node_count = Count("node_id", {"field": None, "primaryField": None, "id": 0})
+    logical_node_count = Count(node_id=0, field=None, primary_field=None)
     sycamore_operator = SycamoreCount(context, logical_node_count, query_id="test", inputs=[doc_set])
     count_result = sycamore_operator.execute()
 
@@ -154,9 +154,7 @@ def test_count_distinct():
 
     return_value_count_distinct = 6
     doc_set.count_distinct.return_value = return_value_count_distinct
-    logical_node_count_distinct = Count(
-        "node_id", {"field": "properties.counter", "primaryField": "text_representation", "id": 0}
-    )
+    logical_node_count_distinct = Count(node_id=0, field="properties.counter", primary_field="text_representation")
     sycamore_operator = SycamoreCount(context, logical_node_count_distinct, query_id="test", inputs=[doc_set])
     count_distinct_result = sycamore_operator.execute()
 
@@ -173,14 +171,13 @@ def test_count_distinct_primary_field():
 
     return_value_count_distinct_primary = 7
     doc_set.count_distinct.return_value = return_value_count_distinct_primary
-    logical_node_count_distinct_primary = Count(
-        "node_id", {"field": None, "primaryField": "text_representation", "id": 0}
-    )
+    logical_node_count_distinct_primary = Count(node_id=0, field=None, primary_field="text_representation")
+
     sycamore_operator = SycamoreCount(context, logical_node_count_distinct_primary, query_id="test", inputs=[doc_set])
     count_distinct_primary_result = sycamore_operator.execute()
 
     doc_set.count_distinct.assert_called_once_with(
-        field=logical_node_count_distinct_primary.data.get("primaryField"), **sycamore_operator.get_execute_args()
+        field=logical_node_count_distinct_primary.primary_field, **sycamore_operator.get_execute_args()
     )
 
     assert count_distinct_primary_result == return_value_count_distinct_primary

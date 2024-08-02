073c3e99d9aff9def0b6d4863a0a34b73b72dbe6
Matt Welsh <mdw@mdw.la>
2024-08-02 15:12:46 -0700

    Add test for schema fetcher.

---
+++ lib/sycamore/sycamore/tests/unit/query/test_schema.py
@@ -0,0 +1,67 @@
+from unittest.mock import MagicMock
+
+from sycamore.query.schema import OpenSearchSchemaFetcher
+
+
+def test_opensearch_schema():
+    mock_client = MagicMock()
+    mock_client.get_field_mapping.return_value = {
+        "test_index": {
+            "mappings": {
+                "properties.entity.day": {"full_name": "properties.entity.day", "mapping": {"day": {"type": "date"}}},
+                "properties.entity.aircraft": {
+                    "full_name": "properties.entity.aircraft",
+                    "mapping": {
+                        "aircraft": {"type": "text", "fields": {"keyword": {"type": "keyword", "ignore_above": 256}}}
+                    },
+                },
+                "properties.entity.location": {
+                    "full_name": "properties.entity.location",
+                    "mapping": {
+                        "location": {"type": "text", "fields": {"keyword": {"type": "keyword", "ignore_above": 256}}}
+                    },
+                },
+            }
+        }
+    }
+
+    mock_query_executor = MagicMock()
+    # Note that there are no values for 'location' here.
+    mock_query_executor.query.return_value = {
+        "result": {
+            "hits": {
+                "hits": [
+                    {
+                        "_source": {
+                            "properties": {
+                                "entity": {
+                                    "day": "2021-01-01",
+                                    "aircraft": "Boeing 747",
+                                }
+                            }
+                        }
+                    },
+                    {
+                        "_source": {
+                            "properties": {
+                                "entity": {
+                                    "day": "2021-01-02",
+                                    "aircraft": "Airbus A380",
+                                }
+                            }
+                        }
+                    },
+                ]
+            }
+        }
+    }
+
+    fetcher = OpenSearchSchemaFetcher(mock_client, "test_index", mock_query_executor)
+    got = fetcher.get_schema()
+    assert "text_representation" in got
+    assert got["text_representation"] == ("<class 'str'>", {"Can be assumed to have all other details"})
+    assert "properties.entity.day" in got
+    assert got["properties.entity.day"] == ("<class 'str'>", {"2021-01-01", "2021-01-02"})
+    assert "properties.entity.aircraft" in got
+    assert got["properties.entity.aircraft"] == ("<class 'str'>", {"Boeing 747", "Airbus A380"})
+    assert "properties.entity.location" not in got

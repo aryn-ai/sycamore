1ece067334680a079abdf16f5ec3b99dc2eee6d6
Vinayak Thapliyal <vinayak@aryn.ai>
2024-08-08 13:00:35 -0700

    Bugfix for query dry-run mode

--- lib/sycamore/sycamore/tests/integration/query/execution/test_sycamore_query.py
+++ lib/sycamore/sycamore/tests/integration/query/execution/test_sycamore_query.py
@@ -31,3 +31,25 @@ class TestSycamoreQuery:
         assert isinstance(result, str)
         assert len(result) > 0
         assert "0" in result
+
+    @pytest.mark.parametrize("dry_run", [True, False])
+    def test_dry_run(self, query_integration_test_index: str, dry_run: bool):
+        """
+        Test that asserts dry_run mode doesn't execute the query. We use ray initialization as a proxy here,
+        we can also look at LLM calls but this is a little more reliable in the current state.
+        """
+        import ray
+
+        client = SycamoreQueryClient()
+        schema = client.get_opensearch_schema(query_integration_test_index)
+        plan = client.generate_plan(
+            "What fraction of all incidents happened in california?", query_integration_test_index, schema
+        )
+        ray.shutdown()
+        query_id, result = client.run_plan(plan, dry_run=dry_run)
+        assert isinstance(result, str)
+        assert len(result) > 0
+        if dry_run:
+            assert not ray.is_initialized()
+        else:
+            assert ray.is_initialized()

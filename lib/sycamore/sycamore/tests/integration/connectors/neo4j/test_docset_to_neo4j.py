89a9b408ede23b1faaab86fb2cb1e68edadbb710
Ritam Saha <ritam@aryn.ai>
2024-08-08 17:16:34 +0000

    test nested func

--- lib/sycamore/sycamore/tests/integration/connectors/neo4j/test_docset_to_neo4j.py
+++ lib/sycamore/sycamore/tests/integration/connectors/neo4j/test_docset_to_neo4j.py
@@ -10,80 +10,12 @@ import os
 from sycamore.transforms.partition import ArynPartitioner
 
 
-########## THESE FUNCTIONS NEED A HOME
-def restructure_doc(doc: Document) -> HierarchicalDocument:
-    doc = HierarchicalDocument(doc.data)     
-    return doc
+def test_to_neo4j():
 
-def children_to_section(doc: HierarchicalDocument) -> HierarchicalDocument:
-    import uuid
+    def restructure_doc(doc: Document) -> HierarchicalDocument:
+        doc = HierarchicalDocument(doc.data)     
+        return doc
     
-    #if the first element is not a section header, insert generic placeholder
-    if(len(doc.children) > 0 and doc.children[0]["type"] != 'Section-header'):
-        initial_page = HierarchicalDocument({'type': 'Section-header', 'bbox': (0, 0, 0, 0), 'properties': {'score': 1, 'page_number': 1}, 'text_representation': 'Front Page', 'binary_representation': b'Front Page'})
-        doc.children.insert(0,initial_page) # O(n) insert :( we should use deque for everything
-
-    if 'relationships' not in doc.data:
-        doc.data['relationships'] = {}
-    if 'label' not in doc.data:
-        doc.data['label'] = 'DOCUMENT'
-
-    sections = []
-
-    section: HierarchicalDocument = None
-    element: HierarchicalDocument = None
-    for child in doc.children:
-        if 'relationships' not in child.data:
-            child.data['relationships'] = {}
-        if child.type == 'Section-header' and 'text_representation' in child.data and len(child.data["text_representation"]) > 0:
-            if section != None:
-                next = {'TYPE':'NEXT',
-                        'properties': {},
-                        'START_ID': section.doc_id,
-                        'START_LABEL':'SECTION',
-                        'END_ID':child.doc_id,
-                        'END_LABEL':'SECTION',
-                        }
-                child.data['relationships'][str(uuid.uuid4())] = next
-                element = None
-            rel = {'TYPE':'SECTION_OF',
-                        'properties': {},
-                        'START_ID': child.doc_id,
-                        'START_LABEL': 'SECTION',
-                        'END_ID':doc.doc_id,
-                        'END_LABEL': 'DOCUMENT'
-                        }
-            child.data['relationships'][str(uuid.uuid4())] = rel
-            child.data['label'] = 'SECTION'
-            section = child
-            sections.append(section)
-        else:
-            if element != None:
-                next = {'TYPE':'NEXT',
-                        'properties': {},
-                        'START_ID': element.doc_id,
-                        'START_LABEL': 'ELEMENT',
-                        'END_ID':child.doc_id,
-                        'END_LABEL': 'ELEMENT'
-                        }
-                child.data['relationships'][str(uuid.uuid4())] = next
-            rel = {'TYPE':'PART_OF',
-                        'properties': {},
-                        'START_ID': child.doc_id,
-                        'START_LABEL': 'ELEMENT',
-                        'END_ID': section.doc_id,
-                        'END_LABEL': 'SECTION'
-                        }
-            child.data['relationships'][str(uuid.uuid4())] = rel
-            child.data['label'] = 'ELEMENT'
-            element = child
-            section.data["children"].append(element)
-
-    doc.children = sections
-    return doc
-##########
-    
-def pdf_to_neo4j():
     path = str(TEST_DIR / "resources/data/pdfs/Ray_page11.pdf")
     context = sycamore.init()
     #URI = "neo4j://localhost:7687"
@@ -94,11 +26,82 @@ def pdf_to_neo4j():
         context.read.binary(path, binary_format="pdf")
         .partition(partitioner=ArynPartitioner(extract_table_structure=True, use_ocr=True, extract_images=True, aryn_api_key=api_key))
         .map(restructure_doc)
-        .map(children_to_section)
+        #.map(children_to_section)
         .explode()
     )
 
     ds.take_all()
 
     #ds.write.neo4j(uri=URI,auth=AUTH,database="neo4j",import_dir="/neo4j/import")
+
+# ########## THESE FUNCTIONS NEED A HOME
+
+
+# def children_to_section(doc: HierarchicalDocument) -> HierarchicalDocument:
+#     import uuid
+    
+#     #if the first element is not a section header, insert generic placeholder
+#     if(len(doc.children) > 0 and doc.children[0]["type"] != 'Section-header'):
+#         initial_page = HierarchicalDocument({'type': 'Section-header', 'bbox': (0, 0, 0, 0), 'properties': {'score': 1, 'page_number': 1}, 'text_representation': 'Front Page', 'binary_representation': b'Front Page'})
+#         doc.children.insert(0,initial_page) # O(n) insert :( we should use deque for everything
+
+#     if 'relationships' not in doc.data:
+#         doc.data['relationships'] = {}
+#     if 'label' not in doc.data:
+#         doc.data['label'] = 'DOCUMENT'
+
+#     sections = []
+
+#     section: HierarchicalDocument = None
+#     element: HierarchicalDocument = None
+#     for child in doc.children:
+#         if 'relationships' not in child.data:
+#             child.data['relationships'] = {}
+#         if child.type == 'Section-header' and 'text_representation' in child.data and len(child.data["text_representation"]) > 0:
+#             if section != None:
+#                 next = {'TYPE':'NEXT',
+#                         'properties': {},
+#                         'START_ID': section.doc_id,
+#                         'START_LABEL':'SECTION',
+#                         'END_ID':child.doc_id,
+#                         'END_LABEL':'SECTION',
+#                         }
+#                 child.data['relationships'][str(uuid.uuid4())] = next
+#                 element = None
+#             rel = {'TYPE':'SECTION_OF',
+#                         'properties': {},
+#                         'START_ID': child.doc_id,
+#                         'START_LABEL': 'SECTION',
+#                         'END_ID':doc.doc_id,
+#                         'END_LABEL': 'DOCUMENT'
+#                         }
+#             child.data['relationships'][str(uuid.uuid4())] = rel
+#             child.data['label'] = 'SECTION'
+#             section = child
+#             sections.append(section)
+#         else:
+#             if element != None:
+#                 next = {'TYPE':'NEXT',
+#                         'properties': {},
+#                         'START_ID': element.doc_id,
+#                         'START_LABEL': 'ELEMENT',
+#                         'END_ID':child.doc_id,
+#                         'END_LABEL': 'ELEMENT'
+#                         }
+#                 child.data['relationships'][str(uuid.uuid4())] = next
+#             rel = {'TYPE':'PART_OF',
+#                         'properties': {},
+#                         'START_ID': child.doc_id,
+#                         'START_LABEL': 'ELEMENT',
+#                         'END_ID': section.doc_id,
+#                         'END_LABEL': 'SECTION'
+#                         }
+#             child.data['relationships'][str(uuid.uuid4())] = rel
+#             child.data['label'] = 'ELEMENT'
+#             element = child
+#             section.data["children"].append(element)
+
+#     doc.children = sections
+#     return doc
+# ##########    
 

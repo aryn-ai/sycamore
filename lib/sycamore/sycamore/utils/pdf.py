3f985f5352413b4cfbde0d58516ea5fc2edafee8
Austin Lee <austin@aryn.ai>
2024-08-09 16:28:41 -0700

    Add support for stdin for pdf_to_images.
    Signed-off-by: Austin Lee <austin@aryn.ai>

--- lib/sycamore/sycamore/utils/pdf.py
+++ lib/sycamore/sycamore/utils/pdf.py
@@ -125,11 +125,17 @@ def convert_from_path_streamed_batched(filename: str, batch_size: int) -> Genera
         yield batch
 
 
-def pdf_to_image_files(pdf_path: str, file_dir: Path) -> Generator[Path, None, None]:
+def pdf_to_image_files(pdf_path: str, file_dir: Path, fh = None, pages = None) -> Generator[Path, None, None]:
     """Writes the files (streamed) into file_dir.  Caller is responsible for calling
     path.unlink() to cleanup the files.
 
     Note: model service will call this to get images for processing"""
+
+    if pdf_path == '-' and fh is None:
+        raise ValueError("fh must be passed when pdf_path is set to '-'")
+
+    if pages:
+        logging.info(f"Processing selected pages {pages}")
 
     class StdoutEOF:
         pass
@@ -171,10 +177,13 @@ def pdf_to_image_files(pdf_path: str, file_dir: Path) -> Generator[Path, None, N
             if len(data) < need_bytes:
                 continue
 
-            out_path = file_dir / f"image.{image_num}.ppm"
-            with open(out_path, "wb") as file:
-                file.write(data[0:need_bytes])
-            q.put(out_path)
+            if pages is None or (pages and (image_num + 1) in pages):
+                if pages:
+                    logging.info(f"Writing selected page {image_num + 1}")
+                out_path = file_dir / f"image.{image_num}.ppm"
+                with open(out_path, "wb") as file:
+                    file.write(data[0:need_bytes])
+                q.put(out_path)
 
             image_num = image_num + 1
 
@@ -195,7 +204,10 @@ def pdf_to_image_files(pdf_path: str, file_dir: Path) -> Generator[Path, None, N
         # If we don't have the separate threads for reading stdout/stderr,
         # then if the stderr buffer fills up we could get stuck.
         args = ["pdftoppm", "-r", "200", pdf_path]
-        proc = Popen(args, stdout=PIPE, stderr=PIPE)
+        if pdf_path == '-':
+            proc = Popen(args, stdin=fh, stdout=PIPE, stderr=PIPE)
+        else:
+            proc = Popen(args, stdout=PIPE, stderr=PIPE)
         q: Queue = Queue(1)
         t_out = Thread(
             target=capture_exception, daemon=True, args=(q, lambda: read_stdout(proc.stdout, q), StdoutEOF())

de8dc62f06e11c59db621cf0befc94a13590389a
Ritam Saha <ritam@aryn.ai>
2024-08-14 16:59:53 +0000

    mypy

--- lib/sycamore/sycamore/llms/openai.py
+++ lib/sycamore/sycamore/llms/openai.py
@@ -419,7 +419,7 @@ class OpenAI(LLM):
             completion = self.client_wrapper.get_client().chat.completions.create(model=self._model_name, **kwargs)
             return completion.choices[0].message.content
 
-    async def generate_async(self, *, prompt_kwargs: dict, llm_kwargs: Optional[dict] = None) -> Awaitable[Any]:
+    async def generate_async(self, *, prompt_kwargs: dict, llm_kwargs: Optional[dict] = None) -> Any:
         key, ret = self._cache_get(prompt_kwargs, llm_kwargs)
         if ret is not None:
             return ret
@@ -437,7 +437,7 @@ class OpenAI(LLM):
         self._cache_set(key, value)
         return ret
 
-    async def _generate_awaitable_using_openai(self, prompt_kwargs, llm_kwargs) -> Awaitable[Any]:
+    async def _generate_awaitable_using_openai(self, prompt_kwargs, llm_kwargs) -> Any:
         kwargs = self._get_generate_kwargs(prompt_kwargs, llm_kwargs)
         if self._determine_using_beta(llm_kwargs.get("response_format", None)):
             completion = await self.client_wrapper.get_async_client().beta.chat.completions.parse(

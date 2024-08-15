1574ffc80e045b50c60bed35f13e14ef6399b9fa
Ritam Saha <ritam@aryn.ai>
2024-08-15 23:39:24 +0000

    test pickling

---
+++ lib/sycamore/sycamore/tests/unit/utils/test_pydantic_pickling.py
@@ -0,0 +1,28 @@
+import sys
+from typing import Optional
+from sycamore.utils.pickle_pydantic import safe_cloudpickle, safe_cloudunpickle
+import pydantic
+from pydantic import BaseModel, Field
+
+
+def test_pydantic_picklng():
+    class BoardMember(BaseModel):
+        name: str
+        votes_for: Optional[int]
+        votes_against: Optional[int]
+        votes_abstentions: Optional[int]
+
+    class Company(BaseModel):
+        name: str
+        founded: Optional[str] = Field(description="The date a company was founded")
+
+    company_pickled = safe_cloudpickle(Company)
+    board_member_pickled = safe_cloudpickle(BoardMember)
+
+    company_unpickled = safe_cloudunpickle(company_pickled)
+    board_member_unpickled = safe_cloudunpickle(board_member_pickled)
+
+
+    assert sys.getsizeof(company_unpickled) == sys.getsizeof(Company)
+    assert sys.getsizeof(board_member_unpickled) == sys.getsizeof(BoardMember)
+

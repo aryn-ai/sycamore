8ee13353c8e902bb3798f73ac80ee2f491abf3e7
Ritam Saha <ritam@aryn.ai>
2024-08-15 22:31:46 +0000

    testing mypy

--- lib/sycamore/sycamore/llms/openai.py
+++ lib/sycamore/sycamore/llms/openai.py
@@ -5,7 +5,7 @@ import os
 import pickle
 from dataclasses import dataclass
 from enum import Enum
-from typing import Any, Optional, TypedDict, Union, cast
+from typing import Any, Awaitable, Optional, TypedDict, Union, cast
 
 from guidance.models import AzureOpenAIChat, AzureOpenAICompletion
 from guidance.models import Model
@@ -420,7 +420,7 @@ class OpenAI(LLM):
             completion = self.client_wrapper.get_client().chat.completions.create(model=self._model_name, **kwargs)
             return completion.choices[0].message.content
 
-    async def generate_async(self, *, prompt_kwargs: dict, llm_kwargs: Optional[dict] = None) -> Any:
+    async def generate_async(self, *, prompt_kwargs: dict, llm_kwargs: Optional[dict] = None) -> Awaitable[str]:
         key, ret = self._cache_get(prompt_kwargs, llm_kwargs)
         if ret is not None:
             return ret
@@ -438,12 +438,13 @@ class OpenAI(LLM):
         self._cache_set(key, value)
         return ret
 
-    async def _generate_awaitable_using_openai(self, prompt_kwargs, llm_kwargs) -> Any:
+    async def _generate_awaitable_using_openai(self, prompt_kwargs, llm_kwargs) -> Awaitable[str]:
         kwargs = self._get_generate_kwargs(prompt_kwargs, llm_kwargs)
         if self._determine_using_beta(llm_kwargs.get("response_format", None)):
             completion = await self.client_wrapper.get_async_client().beta.chat.completions.parse(
                 model=self._model_name, **kwargs
             )
+            assert completion.choices[0].message.content is not None
             return completion.choices[0].message.content
         else:
             completion = await self.client_wrapper.get_async_client().chat.completions.create(

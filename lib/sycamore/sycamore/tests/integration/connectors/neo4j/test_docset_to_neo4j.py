dd7baab391a215a57e3af201fa6500874cc18893
Ritam Saha <ritam@aryn.ai>
2024-08-21 21:24:03 +0000

    updated tests

--- lib/sycamore/sycamore/tests/integration/connectors/neo4j/test_docset_to_neo4j.py
+++ lib/sycamore/sycamore/tests/integration/connectors/neo4j/test_docset_to_neo4j.py
@@ -1,16 +1,43 @@
+from openai import OpenAI
+from pydantic import BaseModel
 import sycamore
+from sycamore.llms.openai import OpenAIModel
 from sycamore.tests.config import TEST_DIR
 from sycamore.transforms.partition import SycamorePartitioner
 from sycamore.transforms.extract_document_structure import StructureBySection
+from sycamore.transforms.extract_graph_entities import EntityExtractor
+from sycamore.transforms.extract_graph_relationships import RelationshipExtractor
 
 
 def test_to_neo4j():
     ## actual test ##
-    path = str(TEST_DIR / "resources/data/pdfs/Ray_page11.pdf")
+    path = str(TEST_DIR / "resources/data/pdfs/doctor_testimonial.pdf")
     context = sycamore.init()
+    model = OpenAIModel(name="gpt-4o-2024-08-06", is_chat=True)
+    llm = OpenAI(model)
+
     URI = "neo4j://localhost:7687"
     AUTH = None
     DATABASE = "neo4j"
+
+    class Doctor(BaseModel):
+        name: str
+
+    class Response(BaseModel):
+        response: str
+        rating: int
+
+    class MarketingMessage(BaseModel):
+        drug_name: str
+        message: str
+
+    class Rated(BaseModel):
+        start: Response
+        end: MarketingMessage
+
+    class Said(BaseModel):
+        start: Doctor
+        end: Response
 
     ds = (
         context.read.binary(path, binary_format="pdf")
@@ -19,8 +46,8 @@ def test_to_neo4j():
             num_gpus=0.2,
         )
         .extract_document_structure(structure=StructureBySection)
-        .extract_graph_entities(extractors=[])
-        .extract_graph_relationships(extractors=[])
+        .extract_graph_entities([EntityExtractor(llm=llm, entities=[Doctor, Response, MarketingMessage])])
+        .extract_graph_relationships(RelationshipExtractor(llm=llm, entities=[Rated, Said]))
         .resolve_graph_entities(resolvers=[])
         .explode()
     )
@@ -33,20 +60,44 @@ def test_to_neo4j():
     session = driver.session(database=DATABASE)
 
     query1 = """
-    MATCH (n:ELEMENT {type: 'table'})
-    RETURN COUNT(n) AS numTables
+    MATCH (n:Doctor)
+    RETURN COUNT(n) AS numDoctors
     """
+
     query2 = """
-    MATCH (n:DOCUMENT)
-    RETURN COUNT(n) AS numDocuments
+    MATCH (n:Response)
+    RETURN COUNT(n) AS numResponses
     """
+
     query3 = """
-    MATCH (n:SECTION)
-    RETURN COUNT(n) AS numSections
+    MATCH (n:MarketingMessage)
+    RETURN COUNT(n) AS numMessages
     """
-    assert session.run(query1).single()["numTables"] == 2
-    assert session.run(query2).single()["numDocuments"] == 1
-    assert session.run(query3).single()["numSections"] == 4
+
+    query4 = """
+    MATCH (d:Doctor)-[r]->(mr:MarketingResponse)
+    RETURN count(r) AS relationshipCount
+    """
+
+    query5 = """
+    MATCH (d:Doctor)-[r]->(mr:Response)
+    RETURN count(r) AS relationshipCount
+    """
+
+    query6 = """
+    MATCH (re:Response)-[r]->(mr:MarketingResponse)
+    RETURN count(r) AS relationshipCount
+    """
+
+    #test nodes
+    assert session.run(query1).single()["numDoctors"] == 1
+    assert session.run(query2).single()["numResponses"] == 2
+    assert session.run(query3).single()["numMessages"] == 2
+
+    #test relationships
+    assert session.run(query4).single()["relationshipCount"] == 0
+    assert session.run(query5).single()["relationshipCount"] == 2
+    assert session.run(query6).single()["relationshipCount"] == 2
 
     session.close()
     driver.close()

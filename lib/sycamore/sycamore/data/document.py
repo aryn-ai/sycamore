5474eff438975fca5cfc1d2f74fd00e113c8e05e
Ritam Saha <ritam@aryn.ai>
2024-08-28 23:52:03 +0000

    check for text_representation instead of _override_text

--- lib/sycamore/sycamore/data/document.py
+++ lib/sycamore/sycamore/data/document.py
@@ -338,10 +338,8 @@ class HierarchicalDocument(Document):
         self.doc_id = self.data.get("doc_id", str(uuid.uuid4()))
         self.children = self.data.get("children", [])
         if self.data.get("type", None) == "table":
-            table_csv = ""
-            if self.data.get("table", None):
-                table_csv = self.data["table"].to_csv()
-            self.text_representation = self.data.get("override_text", table_csv)
+            table_csv = self.data.get("table").to_csv() if self.data.get("table") else ""
+            self.text_representation = self.data.get("text_representation", table_csv)
 
         for element in self.data.get("elements", []):
             self.children.append(HierarchicalDocument(Document(element.data)))

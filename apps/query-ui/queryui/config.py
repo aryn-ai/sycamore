ddc4e9c932590f45e60bbb8423d2e3d1aa587616
Vinayak Thapliyal <vinayak@aryn.ai>
2024-09-07 13:03:56 -0600

    mypy

--- apps/query-ui/queryui/config.py
+++ apps/query-ui/queryui/config.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.INFO)
 If you want to use custom LLMs (or just a custom context in general, you can create a file called custom_client.py
 and overload 
     code:
-        def get_sycamore_query_client(llm, s3_cache_path, os_client_args, trace_dir, **kwargs) -> Context:
+        def get_custom_sycamore_query_client(llm, s3_cache_path, os_client_args, trace_dir, **kwargs) -> Context:
 """
 
 
@@ -21,11 +21,14 @@ def get_default_sycamore_query_client(
     return SycamoreQueryClient(s3_cache_path=s3_cache_path, os_client_args=os_client_args, trace_dir=trace_dir)
 
 
-try:
-    import custom_client
+def get_sycamore_query_client(**kwargs) -> Context:
+    try:
+        import custom_client
 
-    get_sycamore_query_client = custom_client.get_default_sycamore_query_client
-    logging.info("Using custom `get_sycamore_query_client(..)` from custom_client")
-except (ImportError, AttributeError):
-    logging.info("Using default `get_sycamore_query_client(..)`")
-    get_sycamore_query_client = get_default_sycamore_query_client
+        get_sycamore_query_client_impl = custom_client.get_custom_sycamore_query_client
+        logging.info("Using custom `get_custom_sycamore_query_client(..)` from custom_client")
+    except (ImportError, AttributeError):
+        logging.info("Using default `get_sycamore_query_client(..)`")
+        get_sycamore_query_client_impl = get_default_sycamore_query_client
+
+    return get_sycamore_query_client_impl(**kwargs)

b4b09d6693c1d4d6e13cf15757c0991c0993bda8
Karan Sampath <176953591+karanataryn@users.noreply.github.com>
2024-09-20 11:46:41 -0700

    Make PDFMiner Pipelined (#807)
    * make pdfminer pipelined
    
    * fix lint
    
    * remove sequenced tests
    
    * wrong file
    
    * post ben comments
    
    * remove batch at a time
    
    * after ben comments
    
    * fix final layout
    
    * add todo

--- lib/sycamore/sycamore/tests/unit/transforms/check_partition_impl.py
+++ lib/sycamore/sycamore/tests/unit/transforms/check_partition_impl.py
@@ -1,22 +1,18 @@
 from sycamore.utils.cache import Cache
-from sycamore.utils.deep_eq import deep_eq
 
 
-def compare_batched_sequenced(partitioner, path, **kwargs):
+def check_partition(partitioner, path, **kwargs):
     with open(path, "rb") as f:
-        sequenced = partitioner._partition_pdf_sequenced(f, **kwargs)
         hash_key = Cache.get_hash_context(f.read()).hexdigest()
     batched = partitioner._partition_pdf_batched_named(path, hash_key, **kwargs)
-    assert deep_eq(batched, sequenced)
+    assert batched is not None
     return batched
 
 
 def check_table_extraction(partitioner, path, **kwargs):
     with open(path, "rb") as f:
-        sequenced = partitioner._partition_pdf_sequenced(f, **kwargs)
         hash_key = Cache.get_hash_context(f.read()).hexdigest()
     batched = partitioner._partition_pdf_batched_named(path, hash_key, **kwargs)
-    assert deep_eq(batched, sequenced)
     assert all(
         (
             d.tokens is not None and all("bbox" in token and isinstance(token["bbox"], list) for token in d.tokens)
@@ -25,15 +21,6 @@ def check_table_extraction(partitioner, path, **kwargs):
         )
         for batched_list in batched
         for d in batched_list
-    )
-    assert all(
-        (
-            d.tokens is not None and all("bbox" in token and isinstance(token["bbox"], list) for token in d.tokens)
-            if d.type == "table"
-            else True
-        )
-        for sequenced_list in sequenced
-        for d in sequenced_list
     )
     return batched
 
@@ -45,5 +32,5 @@ if __name__ == "__main__":
     assert len(sys.argv) == 2, "Usage: cmd <path>"
     s = ArynPDFPartitioner("Aryn/deformable-detr-DocLayNet")
     print(f"Comparing processing of {sys.argv[1]}")
-    p = compare_batched_sequenced(s, sys.argv[1])
+    p = check_partition(s, sys.argv[1])
     print(f"Compared {len(p)} pages")

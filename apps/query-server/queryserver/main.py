27c6faefa584077ef9fe606aeece6ef67b8f8910
Matt Welsh <mdw@mdw.la>
2024-09-30 17:04:23 -0700

    Basic API is in place.

--- apps/query-server/queryserver/main.py
+++ apps/query-server/queryserver/main.py
@@ -1,10 +1,16 @@
+# This is a FastAPI server that wraps Sycamore Query in a REST service.
+#
+# Run with:
+#   poetry run fastapi dev queryserver/main.py
+
 import os
 import tempfile
-from typing import Annotated, List, Optional
+from typing import Annotated, Any, Dict, List, Optional
 
 from fastapi import FastAPI, Path
 from pydantic import BaseModel
-from sycamore.query import SycamoreQueryClient
+from sycamore.query.client import SycamoreQueryClient
+from sycamore.query.logical_plan import LogicalPlan
 
 import queryserver.util as util
 
@@ -15,7 +21,7 @@ app = FastAPI()
 CACHE_PATH = os.getenv("QUERYSERVER_CACHE_PATH", os.path.join(tempfile.gettempdir(), "queryserver_cache"))
 LLM_CACHE_PATH = os.getenv("QUERYSERVER_LLM_CACHE_PATH", os.path.join(tempfile.gettempdir(), "queryserver_llm_cache"))
 
-client = SycamoreQueryClient(s3_cache_path=LLM_CACHE_PATH, cache_dir=CACHE_PATH)
+sqclient = SycamoreQueryClient(s3_cache_path=LLM_CACHE_PATH, cache_dir=CACHE_PATH)
 
 
 def get_sycamore_query_client(
@@ -24,41 +30,100 @@ def get_sycamore_query_client(
     pass
 
 
+class IndexSchemaField(BaseModel):
+    """Represents a single field in an index schema."""
+
+    field_type: str
+    examples: Optional[List[str]]
+
+
+class IndexSchema(BaseModel):
+    """Represents the schema for a given index."""
+
+    fields: Dict[str, IndexSchemaField] = {}
+
+
 class Index(BaseModel):
-    index_id: str
+    """Represents an index that can be queried."""
+
+    index: str
     description: Optional[str] = None
-    columns: List[str]
+    schema: IndexSchema
 
 
 class Query(BaseModel):
+    """Query an index with a given natural language query string."""
+
     query: str
     index: str
-    examples: Optional[List[str]] = None
 
 
+class QueryResult(BaseModel):
+    """Represents the result of a query operation."""
+
+    plan: LogicalPlan
+    result: Any
+
+
+def get_index_schema(index: str) -> IndexSchema:
+    """Get the schema for the given index."""
+
+    index_schema = IndexSchema()
+    schema = sqclient.get_opensearch_schema(index)
+    for field in schema:
+        index_schema.fields[field] = IndexSchemaField(field_type=schema[field][0], examples=schema[field][1])
+    return index_schema
 
 
 @app.get("/v1/indices")
 async def list_indices() -> List[Index]:
-    indices = get_opensearch_indices()
+    """List all available indices."""
 
-    return {x for x in SycamoreQueryClient().get_opensearch_incides() if not x.startswith(".")}
+    retval = []
+    indices = util.get_opensearch_indices()
+    for index in indices:
+        index_schema = IndexSchema()
+        schema = sqclient.get_opensearch_schema(index)
+        for field in schema:
+            index_schema.fields[field] = IndexSchemaField(field_type=schema[field][0], examples=schema[field][1])
+        retval.append(Index(index=index, schema=index_schema))
+    return retval
 
-    return [
-        Index(index_id="products", columns=["name", "description", "price", "tax"]),
-        Index(index_id="customers", columns=["name", "email", "phone"]),
-    ]
 
-
-@app.get("/v1/index/{index_id}")
+@app.get("/v1/index/{index}")
 async def get_index(
-    index_id: Annotated[str, Path(title="The ID of the index to get")],
-):
-    return Index(index_id=index_id, columns=["name", "description", "price", "tax"])
+    index: Annotated[str, Path(title="The ID of the index to get")],
+) -> Index:
+    """Return details on the given index."""
+
+    schema = get_index_schema(index)
+    return Index(index=index, schema=schema)
 
 
 @app.post("/v1/plan")
-async def generate_plan(
-    index_id: Annotated[str, Path(title="The ID of the index to get")],
-):
-    return Index(index_id=index_id, columns=["name", "description", "price", "tax"])
+async def generate_plan(query: Query) -> LogicalPlan:
+    """Generate a query plan for the given query, but do not run it."""
+
+    plan = sqclient.generate_plan(
+        query.query, query.index, util.get_schema(sqclient, query.index), examples=query.examples
+    )
+    return plan
+
+
+@app.post("/v1/plan/run")
+async def run_plan(plan: LogicalPlan) -> QueryResult:
+    """Run the provided query plan."""
+
+    _, result = sqclient.run_plan(plan)
+    return QueryResult(plan=plan, result=result)
+
+
+@app.post("/v1/query")
+async def run_query(query: Query) -> QueryResult:
+    """Generate a plan for the given query, run it, and return the result."""
+
+    plan = sqclient.generate_plan(
+        query.query, query.index, util.get_schema(sqclient, query.index), examples=query.examples
+    )
+    _, result = sqclient.run_plan(plan)
+    return QueryResult(plan=plan, result=result)

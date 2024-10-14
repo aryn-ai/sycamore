748e443f472d255a27a77f8c556e434e3071386c
Dan Tecuci <dan@aryn.ai>
2024-10-14 12:13:59 -0500

    resolved conflicts

--- lib/sycamore/sycamore/query/client.py
+++ lib/sycamore/sycamore/query/client.py
@@ -306,19 +306,8 @@ def main():
     parser.add_argument("query", type=str, help="Query to run against the index.", nargs="?", default=None)
     parser.add_argument("--show-indices", action="store_true", help="Show all indices")
     parser.add_argument("--index", type=str, help="Index name")
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     parser.add_argument("--schema-file", type=str, help="Schema file")
-=======
-    parser.add_argument("--schema-file", type=str, help="Schema File")   
->>>>>>> cf46af0f (added ability to read schema from file)
-=======
-    parser.add_argument("--schema-file", type=str, help="Schema file")   
->>>>>>> 5e8b93f1 (small typo)
-=======
-    parser.add_argument("--schema-file", type=str, help="Schema file")
->>>>>>> 08f2d3d3 (reformatted file with black)
+
     parser.add_argument(
         "--s3-cache-path",
         type=str,

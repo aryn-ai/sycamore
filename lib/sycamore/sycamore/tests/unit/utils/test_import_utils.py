9c0817921cee63c5c3d3ff5d12518bc23e69df06
Matt Welsh <mdw@mdw.la>
2024-10-17 17:10:20 -0700

    Fix mypy warnings.

--- lib/sycamore/sycamore/tests/unit/utils/test_import_utils.py
+++ lib/sycamore/sycamore/tests/unit/utils/test_import_utils.py
@@ -1,5 +1,4 @@
-from typing import TYPE_CHECKING
-from mypy import api
+from typing import assert_type
 from sycamore.utils.import_utils import requires_modules
 
 
@@ -9,13 +8,8 @@ def require_fn() -> int:
     return 42
 
 
-res = require_fn()
-if TYPE_CHECKING:
-    reveal_type(res)  # noqa: F821
-
-
 # This test fails prior to adding generic (ParamSpec and TypeVar) type annotations to the
 # requires_modules decorator, as the revealed type is "Any".
-def test_mypy_type():
-    mypy_res = api.run([__file__])
-    assert 'Revealed type is "builtins.int"' in mypy_res[0]
+def test_mypy_type() -> None:
+    res = require_fn()
+    assert_type(res, int)

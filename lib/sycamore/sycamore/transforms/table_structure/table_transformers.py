7e6b62639ce9b8f63d56cb35a32837d1c97e711e
Henry Lindeman <hmlindeman@yahoo.com>
2024-12-04 14:15:38 -0800

    Add deformable table extractor (#1053)
    * add deformable table extractor
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    * add docstrings
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>
    
    ---------
    
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>

--- lib/sycamore/sycamore/transforms/table_structure/table_transformers.py
+++ lib/sycamore/sycamore/transforms/table_structure/table_transformers.py
@@ -75,12 +75,17 @@ def rescale_bboxes(out_bbox, size):
     return b
 
 
-def outputs_to_objects(outputs, img_size, id2label):
+def outputs_to_objects(outputs, img_size, id2label, apply_thresholds: bool = False):
     m = outputs.logits.softmax(-1).max(-1)
     pred_labels = list(m.indices.detach().cpu().numpy())[0]
     pred_scores = list(m.values.detach().cpu().numpy())[0]
     pred_bboxes = outputs["pred_boxes"].detach().cpu()[0]
     pred_bboxes = [elem.tolist() for elem in rescale_bboxes(pred_bboxes, img_size)]
+
+    if apply_thresholds:
+        pred_bboxes, pred_scores, pred_labels = apply_class_thresholds(
+            pred_bboxes, pred_labels, pred_scores, id2label, DEFAULT_STRUCTURE_CLASS_THRESHOLDS
+        )
 
     objects = []
     for label, score, bbox in zip(pred_labels, pred_scores, pred_bboxes):
@@ -906,9 +911,10 @@ def objects_to_structures(objects, tokens, class_thresholds):
     if len(tables) == 0:
         return {}
     if len(tables) > 1:
+        tables.sort(key=lambda x: x["score"], reverse=True)
         import logging
 
-        logging.warning("Got multiple tables in document. Using only the first one")
+        logging.warning("Got multiple tables in document. Using only the highest-scoring one")
 
     table = tables[0]
     structure = {}

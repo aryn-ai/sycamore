30f29d25715a76a4b78cdaf83a31156a614251b4
Mark Lindblad <mark@aryn.ai>
2024-12-05 11:56:02 -0800

    Make clear in docstring promotion behavior is only performed once and on the first page

--- lib/aryn-sdk/aryn_sdk/partition/partition.py
+++ lib/aryn-sdk/aryn_sdk/partition/partition.py
@@ -86,8 +86,11 @@ def partition_file(
         output_format: controls output representation; can be set to "markdown" or "json"
             default: None (JSON elements)
         output_label_options: A dictionary for configuring output label behavior. It supports two options:
-            promote_title, a boolean specifying whether to add a title to partitioned elements if one is missing, and
-            title_candidate_elements, a list of strings representing labels for potential titles.
+            promote_title, a boolean specifying whether to pick the largest element by font size on the first page
+                from among the elements that are of a type specified in title_candidate_elements and promote it to
+                type Title if there is no element on the first page of type Title already.
+            title_candidate_elements, a list of strings representing the labels types allowed to be promoted to
+                a title.
             Here is an example set of output label options:
                 {"promote_title": True, "title_candidate_elements": ["Section-header", "Caption"]}
             default: None (no elements are promoted to titles)

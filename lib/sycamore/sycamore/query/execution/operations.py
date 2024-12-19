14c89e45637b09ef386b50286cf75e188a3ad7f7
Austin Lee <austin@aryn.ai>
2024-12-18 21:59:32 -0800

    Address reviewer comments, fix broken unit test, fix lint

--- lib/sycamore/sycamore/query/execution/operations.py
+++ lib/sycamore/sycamore/query/execution/operations.py
@@ -1,142 +1,25 @@
 import math
-import time
-from typing import Any, List, Union, Optional, Callable
+from typing import Any, List, Union, Optional
 
 import structlog
 
 from sycamore import DocSet
 from sycamore.context import context_params, Context
-from sycamore.data import MetadataDocument, Document
+from sycamore.data import MetadataDocument
 from sycamore.functions import CharacterTokenizer, Tokenizer
 from sycamore.llms.llms import LLM
 from sycamore.llms.prompts.default_prompts import (
     SummarizeDataMessagesPrompt,
 )
-from sycamore.transforms.summarize import Summarizer
+from sycamore.transforms.summarize import (
+    NUM_TEXT_CHARS_GENERATE,
+    DocumentSummarizer,
+    collapse,
+    QuestionAnsweringSummarizer,
+    BASE_PROPS,
+)
 
 log = structlog.get_logger(__name__)
-
-BASE_PROPS = [
-    "filename",
-    "filetype",
-    "page_number",
-    "page_numbers",
-    "links",
-    "element_id",
-    "parent_id",
-    "_schema",
-    "_schema_class",
-    "entity",
-]
-
-NUM_DOCS_GENERATE = 60
-NUM_TEXT_CHARS_GENERATE = 2500
-
-
-class QuestionAnsweringSummarizer:
-    def __init__(self, llm: LLM, question: str):
-        self.llm = llm
-        self.question = question
-
-    def __call__(self, text: str) -> str:
-        messages = SummarizeDataMessagesPrompt(question=self.question, text=text).as_messages()
-        prompt_kwargs = {"messages": messages}
-
-        t0 = time.time()
-        # call to LLM
-        summary = self.llm.generate(prompt_kwargs=prompt_kwargs, llm_kwargs={"temperature": 0})
-        t1 = time.time()
-        log.info(f"Summarizer took {t1 - t0} seconds to generate summary.")
-
-        return summary
-
-
-def collapse(text: str, tokens_per_chunk: int, tokenizer: Tokenizer, summarizer_fn: Callable[[str], str]) -> str:
-    """
-    Collapses text iteratively, summarizing the first chunk and incorporating it in the summary for the next chunk.
-
-    Args:
-        text: Text to collapse.
-        chunk_size: Size of each chunk.
-        tokenizer: Tokenizer to use for counting against max_tokens.
-
-    Returns:
-        List of chunks.
-    """
-    tokens = tokenizer.tokenize(text)
-    total = len(tokens)
-    if total <= tokens_per_chunk:
-        return text
-    done = False
-    i = 0
-    additional = i + tokens_per_chunk
-    cur_summary = ""
-    while not done:
-        input = ""
-        if cur_summary:
-            input = f"{cur_summary}\n"
-        input += "".join(tokens[i : i + additional])
-        print(f"input size: {len(input)}")
-        cur_summary = summarizer_fn(input)
-        assert (
-            len(cur_summary) <= tokens_per_chunk
-        ), f"Summarizer output is longer than input chunk {len(cur_summary)} > {tokens_per_chunk} !!!"
-        print(f"summary to chunk ratio: {len(cur_summary) / tokens_per_chunk}")
-        i += additional
-        remaining = tokens_per_chunk - len(cur_summary)
-        additional = min(remaining, total - i)
-        if additional == 0:
-            break
-
-    return cur_summary
-
-
-class DocumentSummarizer(Summarizer):
-    def __init__(
-        self,
-        llm: LLM,
-        question: str,
-        chunk_size: int = 10 * 1000,
-        chunk_overlap: int = 0,
-        use_elements: bool = False,
-        num_elements: int = 5,
-    ):
-        self.llm = llm
-        self.question = question
-        self.chunk_size = chunk_size
-        self.chunk_overlap = chunk_overlap
-        self.use_elements = use_elements
-        self.num_elements = num_elements
-
-    def summarize(self, document: Document) -> Document:
-        text = self.get_text(document)
-        summary = collapse(
-            text, self.chunk_size, CharacterTokenizer(), QuestionAnsweringSummarizer(self.llm, self.question)
-        )
-        document.properties["summary"] = summary
-        return document
-
-    def get_text(self, doc: Document) -> str:
-        doc_text = ""
-        props_dict = doc.properties.get("entity", {})
-        props_dict.update({p: doc.properties[p] for p in set(doc.properties) - set(BASE_PROPS)})
-        # doc_text = f"Document {di}:\n"
-        for k, v in props_dict.items():
-            doc_text += f"{k}: {v}\n"
-
-        doc_text_representation = ""
-        if not self.use_elements:
-            if doc.text_representation is not None:
-                doc_text_representation += doc.text_representation[:NUM_TEXT_CHARS_GENERATE]
-        else:
-            for element in doc.elements[: self.num_elements]:
-                # Greedy fill doc level text length
-                if len(doc_text_representation) >= NUM_TEXT_CHARS_GENERATE:
-                    break
-                doc_text_representation += (element.text_representation or "") + "\n"
-        doc_text += f"Text contents:\n{doc_text_representation}\n"
-
-        return doc_text
 
 
 def math_operation(val1: int, val2: int, operator: str) -> Union[int, float]:
@@ -286,8 +169,8 @@ def summarize_map_reduce(
     result_data: List[Any],
     use_elements: bool = False,
     num_elements: int = 5,
-    max_tokens: Optional[int] = 10 * 1000,
-    tokenizer: Optional[Tokenizer] = CharacterTokenizer(),
+    max_tokens: int = 10 * 1000,
+    tokenizer: Tokenizer = CharacterTokenizer(),
 ) -> str:
     """ """
     text = f"Data description: {result_description}\n"
--- lib/sycamore/sycamore/transforms/summarize.py
+++ lib/sycamore/sycamore/transforms/summarize.py
@@ -1,13 +1,32 @@
+import logging
+import time
 from abc import ABC, abstractmethod
 from typing import Callable, Optional
 
 
 from sycamore.data import Element, Document
+from sycamore.functions import Tokenizer, CharacterTokenizer
+from sycamore.llms.prompts.default_prompts import SummarizeDataMessagesPrompt
 from sycamore.plan_nodes import NonCPUUser, NonGPUUser, Node
 from sycamore.llms import LLM
 from sycamore.llms.prompts import TextSummarizerGuidancePrompt
 from sycamore.transforms.map import Map
 from sycamore.utils.time_trace import timetrace
+
+NUM_DOCS_GENERATE = 60
+NUM_TEXT_CHARS_GENERATE = 2500
+BASE_PROPS = [
+    "filename",
+    "filetype",
+    "page_number",
+    "page_numbers",
+    "links",
+    "element_id",
+    "parent_id",
+    "_schema",
+    "_schema_class",
+    "entity",
+]
 
 
 class Summarizer(ABC):
@@ -64,6 +83,111 @@ class LLMElementTextSummarizer(Summarizer):
             response = self._llm.generate(prompt_kwargs={"prompt": prompt, "query": element.text_representation})
             element.properties["summary"] = response
         return element
+
+
+class QuestionAnsweringSummarizer:
+    def __init__(self, llm: LLM, question: str):
+        self.llm = llm
+        self.question = question
+
+    def __call__(self, text: str) -> str:
+        messages = SummarizeDataMessagesPrompt(question=self.question, text=text).as_messages()
+        prompt_kwargs = {"messages": messages}
+
+        t0 = time.time()
+        # call to LLM
+        summary = self.llm.generate(prompt_kwargs=prompt_kwargs, llm_kwargs={"temperature": 0})
+        t1 = time.time()
+        logging.info(f"Summarizer took {t1 - t0} seconds to generate summary.")
+
+        return summary
+
+
+def collapse(text: str, tokens_per_chunk: int, tokenizer: Tokenizer, summarizer_fn: Callable[[str], str]) -> str:
+    """
+    Collapses text iteratively, summarizing the first chunk and incorporating it in the summary for the next chunk.
+
+    Args:
+        text: Text to collapse.
+        chunk_size: Size of each chunk.
+        tokenizer: Tokenizer to use for counting against max_tokens.
+
+    Returns:
+        List of chunks.
+    """
+    tokens = tokenizer.tokenize(text)
+    total = len(tokens)
+    if total <= tokens_per_chunk:
+        return text
+    done = False
+    i = 0
+    additional = i + tokens_per_chunk
+    cur_summary = ""
+    while not done:
+        input = ""
+        if cur_summary:
+            input = f"{cur_summary}\n"
+        input += "".join([str(tk) for tk in tokens[i : i + additional]])  # make mypy happy
+        print(f"input size: {len(input)}")
+        cur_summary = summarizer_fn(input)
+        assert (
+            len(cur_summary) <= tokens_per_chunk
+        ), f"Summarizer output is longer than input chunk {len(cur_summary)} > {tokens_per_chunk} !!!"
+        print(f"summary to chunk ratio: {len(cur_summary) / tokens_per_chunk}")
+        i += additional
+        remaining = tokens_per_chunk - len(cur_summary)
+        additional = min(remaining, total - i)
+        if additional == 0:
+            break
+
+    return cur_summary
+
+
+class DocumentSummarizer(Summarizer):
+    def __init__(
+        self,
+        llm: LLM,
+        question: str,
+        chunk_size: int = 10 * 1000,
+        tokenizer: Tokenizer = CharacterTokenizer(),
+        chunk_overlap: int = 0,
+        use_elements: bool = False,
+        num_elements: int = 5,
+    ):
+        self.llm = llm
+        self.question = question
+        self.chunk_size = chunk_size
+        self.tokenizer = tokenizer
+        self.chunk_overlap = chunk_overlap
+        self.use_elements = use_elements
+        self.num_elements = num_elements
+
+    def summarize(self, document: Document) -> Document:
+        text = self.get_text(document)
+        summary = collapse(text, self.chunk_size, self.tokenizer, QuestionAnsweringSummarizer(self.llm, self.question))
+        document.properties["summary"] = summary
+        return document
+
+    def get_text(self, doc: Document) -> str:
+        doc_text = ""
+        props_dict = doc.properties.get("entity", {})
+        props_dict.update({p: doc.properties[p] for p in set(doc.properties) - set(BASE_PROPS)})
+        for k, v in props_dict.items():
+            doc_text += f"{k}: {v}\n"
+
+        doc_text_representation = ""
+        if not self.use_elements:
+            if doc.text_representation is not None:
+                doc_text_representation += doc.text_representation[:NUM_TEXT_CHARS_GENERATE]
+        else:
+            for element in doc.elements[: self.num_elements]:
+                # Greedy fill doc level text length
+                if len(doc_text_representation) >= NUM_TEXT_CHARS_GENERATE:
+                    break
+                doc_text_representation += (element.text_representation or "") + "\n"
+        doc_text += f"Text contents:\n{doc_text_representation}\n"
+
+        return doc_text
 
 
 class Summarize(NonCPUUser, NonGPUUser, Map):

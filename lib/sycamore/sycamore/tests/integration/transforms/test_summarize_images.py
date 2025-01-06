f02d7aafe2593805762e6baa824af960475ac1c8
Ben Sowell <ben@aryn.ai>
2025-01-05 22:11:07 -0800

    Add support for Anthropic LLMs.
    Previously we only supported Claude via Bedrock, but this adds support for
    calling Anthropic directly.
    
    I have tried to factor out common code, though there is likely more that could
    be done. In particular, the Anthropic sdk actually supports Bedrock as well,
    so we could migrate from the AWS SDK, though that may have subtle behavioral
    differences, so I didn't want to make the change here.
    
    While adding tests, I observed that the Bedrock integ tests were failing, so I
    fixed those as well.

--- lib/sycamore/sycamore/tests/integration/transforms/test_summarize_images.py
+++ lib/sycamore/sycamore/tests/integration/transforms/test_summarize_images.py
@@ -1,8 +1,10 @@
 import sycamore
+from sycamore.context import ExecMode
 from sycamore.transforms.partition import ArynPartitioner
 from sycamore.transforms.summarize_images import LLMImageSummarizer, OpenAIImageSummarizer, SummarizeImages
 from sycamore.tests.config import TEST_DIR
 from sycamore.llms.bedrock import BedrockModels, Bedrock
+from sycamore.llms.anthropic import AnthropicModels, Anthropic
 
 
 def test_summarize_images_openai():
@@ -22,12 +24,31 @@ def test_summarize_images_openai():
     assert image_docs[0].properties["summary"]["is_graph"]
 
 
-def test_summarize_images_claude():
+def test_summarize_images_bedrock_claude():
     llm = Bedrock(BedrockModels.CLAUDE_3_5_SONNET)
 
     path = TEST_DIR / "resources/data/pdfs/Ray_page11.pdf"
 
     context = sycamore.init()
+    image_docs = (
+        context.read.binary(paths=[str(path)], binary_format="pdf")
+        .partition(ArynPartitioner(extract_images=True, use_partitioning_service=False, use_cache=False))
+        .transform(SummarizeImages, summarizer=LLMImageSummarizer(llm=llm))
+        .explode()
+        .filter(lambda d: d.type == "Image")
+        .take_all()
+    )
+
+    assert len(image_docs) == 1
+    assert image_docs[0].properties["summary"]["is_graph"]
+
+
+def test_summarize_images_anthropic_claude():
+    llm = Anthropic(AnthropicModels.CLAUDE_3_5_SONNET)
+
+    path = TEST_DIR / "resources/data/pdfs/Ray_page11.pdf"
+
+    context = sycamore.init(exec_mode=ExecMode.LOCAL)
     image_docs = (
         context.read.binary(paths=[str(path)], binary_format="pdf")
         .partition(ArynPartitioner(extract_images=True, use_partitioning_service=False, use_cache=False))

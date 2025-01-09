7d9fa5d3b36f8d0278a400696a19467a772eaef0
Mark Lindblad <mark@aryn.ai>
2025-01-09 10:33:11 -0800

    Add `PartitionError` to `aryn_sdk.partition`'s `__init__.py`

--- lib/aryn-sdk/aryn_sdk/partition/__init__.py
+++ lib/aryn-sdk/aryn_sdk/partition/__init__.py
@@ -1,4 +1,4 @@
-from .partition import partition_file, tables_to_pandas, table_elem_to_dataframe, convert_image_element
+from .partition import partition_file, tables_to_pandas, table_elem_to_dataframe, convert_image_element, PartitionError
 from .art import draw_with_boxes
 
-__all__ = ["partition_file", "table_elem_to_dataframe", "tables_to_pandas", "draw_with_boxes", "convert_image_element"]
+__all__ = ["partition_file", "table_elem_to_dataframe", "tables_to_pandas", "draw_with_boxes", "convert_image_element", "PartitionError"]

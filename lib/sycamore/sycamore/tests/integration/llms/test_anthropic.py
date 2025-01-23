e1b32062dcc3c3bf7631a3085472305e4e2c2662
Henry Lindeman <hmlindeman@yahoo.com>
2025-01-23 13:09:46 -0800

    fix anthropic and bedrock ITs
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>

--- lib/sycamore/sycamore/tests/integration/llms/test_anthropic.py
+++ lib/sycamore/sycamore/tests/integration/llms/test_anthropic.py
@@ -2,14 +2,17 @@ from pathlib import Path
 from typing import Any
 
 from sycamore.llms import Anthropic, AnthropicModels
+from sycamore.llms.prompts.prompts import RenderedPrompt, RenderedMessage
 from sycamore.utils.cache import DiskCache
 
 
 def test_anthropic_defaults():
     llm = Anthropic(AnthropicModels.CLAUDE_3_HAIKU)
-    prompt_kwargs = {"prompt": "Write a limerick about large language models."}
+    prompt = RenderedPrompt(
+        messages=[RenderedMessage(role="user", content="Write a limerick about large language models.")]
+    )
 
-    res = llm.generate(prompt_kwargs=prompt_kwargs, llm_kwargs={})
+    res = llm.generate(prompt=prompt, llm_kwargs={})
 
     assert len(res) > 0
 
@@ -17,14 +20,14 @@ def test_anthropic_defaults():
 def test_anthropic_messages_defaults():
     llm = Anthropic(AnthropicModels.CLAUDE_3_HAIKU)
     messages = [
-        {
-            "role": "user",
-            "content": "Write a caption for a recent trip to a sunny beach",
-        },
+        RenderedMessage(
+            role="user",
+            content="Write a caption for a recent trip to a sunny beach",
+        ),
     ]
-    prompt_kwargs = {"messages": messages}
+    prompt = RenderedPrompt(messages=messages)
 
-    res = llm.generate(prompt_kwargs=prompt_kwargs, llm_kwargs={})
+    res = llm.generate(prompt=prompt, llm_kwargs={})
 
     assert len(res) > 0
 
@@ -32,43 +35,53 @@ def test_anthropic_messages_defaults():
 def test_cached_anthropic(tmp_path: Path):
     cache = DiskCache(str(tmp_path))
     llm = Anthropic(AnthropicModels.CLAUDE_3_HAIKU, cache=cache)
-    prompt_kwargs = {"prompt": "Write a limerick about large language models."}
+    prompt = RenderedPrompt(
+        messages=[RenderedMessage(role="user", content="Write a limerick about large language models.")]
+    )
 
     # pylint: disable=protected-access
-    key = llm._llm_cache_key(prompt_kwargs, {})
+    key = llm._llm_cache_key(prompt, {})
 
-    res = llm.generate(prompt_kwargs=prompt_kwargs, llm_kwargs={})
+    res = llm.generate(prompt=prompt, llm_kwargs={})
 
     # assert result is cached
     assert cache.get(key).get("result")["output"] == res
-    assert cache.get(key).get("prompt_kwargs") == prompt_kwargs
+    assert cache.get(key).get("prompt") == prompt
     assert cache.get(key).get("llm_kwargs") == {}
     assert cache.get(key).get("model_name") == AnthropicModels.CLAUDE_3_HAIKU.value
 
     # assert llm.generate is using cached result
     custom_output: dict[str, Any] = {
         "result": {"output": "This is a custom response"},
-        "prompt_kwargs": prompt_kwargs,
+        "prompt": prompt,
         "llm_kwargs": {},
         "model_name": AnthropicModels.CLAUDE_3_HAIKU.value,
     }
     cache.set(key, custom_output)
 
-    assert llm.generate(prompt_kwargs=prompt_kwargs, llm_kwargs={}) == custom_output["result"]["output"]
+    assert llm.generate(prompt=prompt, llm_kwargs={}) == custom_output["result"]["output"]
 
 
 def test_cached_bedrock_different_prompts(tmp_path: Path):
     cache = DiskCache(str(tmp_path))
     llm = Anthropic(AnthropicModels.CLAUDE_3_HAIKU, cache=cache)
-    prompt_kwargs_1 = {"prompt": "Write a limerick about large language models."}
-    prompt_kwargs_2 = {"prompt": "Write a short limerick about large language models."}
-    prompt_kwargs_3 = {"prompt": "Write a poem about large language models."}
-    prompt_kwargs_4 = {"prompt": "Write a short poem about large language models."}
+    prompt_1 = RenderedPrompt(
+        messages=[RenderedMessage(role="user", content="Write a limerick about large language models.")]
+    )
+    prompt_2 = RenderedPrompt(
+        messages=[RenderedMessage(role="user", content="Write a short limerick about large language models.")]
+    )
+    prompt_3 = RenderedPrompt(
+        messages=[RenderedMessage(role="user", content="Write a poem about large language models.")]
+    )
+    prompt_4 = RenderedPrompt(
+        messages=[RenderedMessage(role="user", content="Write a short poem about large language models.")]
+    )
 
-    key_1 = llm._llm_cache_key(prompt_kwargs_1, {})
-    key_2 = llm._llm_cache_key(prompt_kwargs_2, {})
-    key_3 = llm._llm_cache_key(prompt_kwargs_3, {})
-    key_4 = llm._llm_cache_key(prompt_kwargs_4, {})
+    key_1 = llm._llm_cache_key(prompt_1, {})
+    key_2 = llm._llm_cache_key(prompt_2, {})
+    key_3 = llm._llm_cache_key(prompt_3, {})
+    key_4 = llm._llm_cache_key(prompt_4, {})
     keys = [key_1, key_2, key_3, key_4]
 
     assert len(keys) == len(
@@ -87,21 +100,23 @@ def test_cached_anthropic_different_models(tmp_path: Path):
     llm_HAIKU = Anthropic(AnthropicModels.CLAUDE_3_HAIKU, cache=cache)
     llm_SONNET = Anthropic(AnthropicModels.CLAUDE_3_SONNET, cache=cache)
 
-    prompt_kwargs = {"prompt": "Write a limerick about large language models."}
+    prompt = RenderedPrompt(
+        messages=[RenderedMessage(role="user", content="Write a limerick about large language models.")]
+    )
 
     # populate cache
-    key_HAIKU = llm_HAIKU._llm_cache_key(prompt_kwargs, {})
-    res_HAIKU = llm_HAIKU.generate(prompt_kwargs=prompt_kwargs, llm_kwargs={})
-    key_SONNET = llm_SONNET._llm_cache_key(prompt_kwargs, {})
-    res_SONNET = llm_SONNET.generate(prompt_kwargs=prompt_kwargs, llm_kwargs={})
+    key_HAIKU = llm_HAIKU._llm_cache_key(prompt, {})
+    res_HAIKU = llm_HAIKU.generate(prompt=prompt, llm_kwargs={})
+    key_SONNET = llm_SONNET._llm_cache_key(prompt, {})
+    res_SONNET = llm_SONNET.generate(prompt=prompt, llm_kwargs={})
 
     # check proper cached results
     assert cache.get(key_HAIKU).get("result")["output"] == res_HAIKU
-    assert cache.get(key_HAIKU).get("prompt_kwargs") == prompt_kwargs
+    assert cache.get(key_HAIKU).get("prompt") == prompt
     assert cache.get(key_HAIKU).get("llm_kwargs") == {}
     assert cache.get(key_HAIKU).get("model_name") == AnthropicModels.CLAUDE_3_HAIKU.value
     assert cache.get(key_SONNET).get("result")["output"] == res_SONNET
-    assert cache.get(key_SONNET).get("prompt_kwargs") == prompt_kwargs
+    assert cache.get(key_SONNET).get("prompt") == prompt
     assert cache.get(key_SONNET).get("llm_kwargs") == {}
     assert cache.get(key_SONNET).get("model_name") == AnthropicModels.CLAUDE_3_SONNET.value
 
@@ -112,9 +127,11 @@ def test_cached_anthropic_different_models(tmp_path: Path):
 
 def test_metadata():
     llm = Anthropic(AnthropicModels.CLAUDE_3_HAIKU)
-    prompt_kwargs = {"prompt": "Write a limerick about large language models."}
+    prompt = RenderedPrompt(
+        messages=[RenderedMessage(role="user", content="Write a limerick about large language models.")]
+    )
 
-    res = llm.generate_metadata(prompt_kwargs=prompt_kwargs, llm_kwargs={})
+    res = llm.generate_metadata(prompt=prompt, llm_kwargs={})
 
     assert "output" in res
     assert "wall_latency" in res
--- lib/sycamore/sycamore/tests/integration/llms/test_bedrock.py
+++ lib/sycamore/sycamore/tests/integration/llms/test_bedrock.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Any
 
 from sycamore.llms import Bedrock, BedrockModels
+from sycamore.llms.prompts import RenderedPrompt, RenderedMessage
 from sycamore.utils.cache import DiskCache
 
 
@@ -10,9 +11,11 @@ from sycamore.utils.cache import DiskCache
 
 def test_bedrock_defaults():
     llm = Bedrock(BedrockModels.CLAUDE_3_HAIKU)
-    prompt_kwargs = {"prompt": "Write a limerick about large language models."}
+    prompt = RenderedPrompt(
+        messages=[RenderedMessage(role="user", content="Write a limerick about large language models.")]
+    )
 
-    res = llm.generate(prompt_kwargs=prompt_kwargs, llm_kwargs={})
+    res = llm.generate(prompt=prompt, llm_kwargs={})
 
     assert len(res) > 0
 
@@ -20,14 +23,14 @@ def test_bedrock_defaults():
 def test_bedrock_messages_defaults():
     llm = Bedrock(BedrockModels.CLAUDE_3_HAIKU)
     messages = [
-        {
-            "role": "user",
-            "content": "Write a caption for a recent trip to a sunny beach",
-        },
+        RenderedMessage(
+            role="user",
+            content="Write a caption for a recent trip to a sunny beach",
+        ),
     ]
-    prompt_kwargs = {"messages": messages}
+    prompt = RenderedPrompt(messages=messages)
 
-    res = llm.generate(prompt_kwargs=prompt_kwargs, llm_kwargs={})
+    res = llm.generate(prompt=prompt, llm_kwargs={})
 
     assert len(res) > 0
 
@@ -35,43 +38,53 @@ def test_bedrock_messages_defaults():
 def test_cached_bedrock(tmp_path: Path):
     cache = DiskCache(str(tmp_path))
     llm = Bedrock(BedrockModels.CLAUDE_3_HAIKU, cache=cache)
-    prompt_kwargs = {"prompt": "Write a limerick about large language models."}
+    prompt = RenderedPrompt(
+        messages=[RenderedMessage(role="user", content="Write a limerick about large language models.")]
+    )
 
     # pylint: disable=protected-access
-    key = llm._llm_cache_key(prompt_kwargs, {})
+    key = llm._llm_cache_key(prompt, {})
 
-    res = llm.generate(prompt_kwargs=prompt_kwargs, llm_kwargs={})
+    res = llm.generate(prompt=prompt, llm_kwargs={})
 
     # assert result is cached
     assert cache.get(key).get("result")["output"] == res
-    assert cache.get(key).get("prompt_kwargs") == prompt_kwargs
+    assert cache.get(key).get("prompt") == prompt
     assert cache.get(key).get("llm_kwargs") == {}
     assert cache.get(key).get("model_name") == BedrockModels.CLAUDE_3_HAIKU.value.name
 
     # assert llm.generate is using cached result
     custom_output: dict[str, Any] = {
         "result": {"output": "This is a custom response"},
-        "prompt_kwargs": prompt_kwargs,
+        "prompt": prompt,
         "llm_kwargs": {},
         "model_name": BedrockModels.CLAUDE_3_HAIKU.value.name,
     }
     cache.set(key, custom_output)
 
-    assert llm.generate(prompt_kwargs=prompt_kwargs, llm_kwargs={}) == custom_output["result"]["output"]
+    assert llm.generate(prompt=prompt, llm_kwargs={}) == custom_output["result"]["output"]
 
 
 def test_cached_bedrock_different_prompts(tmp_path: Path):
     cache = DiskCache(str(tmp_path))
     llm = Bedrock(BedrockModels.CLAUDE_3_HAIKU, cache=cache)
-    prompt_kwargs_1 = {"prompt": "Write a limerick about large language models."}
-    prompt_kwargs_2 = {"prompt": "Write a short limerick about large language models."}
-    prompt_kwargs_3 = {"prompt": "Write a poem about large language models."}
-    prompt_kwargs_4 = {"prompt": "Write a short poem about large language models."}
+    prompt_1 = RenderedPrompt(
+        messages=[RenderedMessage(role="user", content="Write a limerick about large language models.")]
+    )
+    prompt_2 = RenderedPrompt(
+        messages=[RenderedMessage(role="user", content="Write a short limerick about large language models.")]
+    )
+    prompt_3 = RenderedPrompt(
+        messages=[RenderedMessage(role="user", content="Write a poem about large language models.")]
+    )
+    prompt_4 = RenderedPrompt(
+        messages=[RenderedMessage(role="user", content="Write a short poem about large language models.")]
+    )
 
-    key_1 = llm._llm_cache_key(prompt_kwargs_1, {})
-    key_2 = llm._llm_cache_key(prompt_kwargs_2, {})
-    key_3 = llm._llm_cache_key(prompt_kwargs_3, {})
-    key_4 = llm._llm_cache_key(prompt_kwargs_4, {})
+    key_1 = llm._llm_cache_key(prompt_1, {})
+    key_2 = llm._llm_cache_key(prompt_2, {})
+    key_3 = llm._llm_cache_key(prompt_3, {})
+    key_4 = llm._llm_cache_key(prompt_4, {})
     keys = [key_1, key_2, key_3, key_4]
 
     assert len(keys) == len(
@@ -90,21 +103,23 @@ def test_cached_bedrock_different_models(tmp_path: Path):
     llm_HAIKU = Bedrock(BedrockModels.CLAUDE_3_HAIKU, cache=cache)
     llm_SONNET = Bedrock(BedrockModels.CLAUDE_3_SONNET, cache=cache)
 
-    prompt_kwargs = {"prompt": "Write a limerick about large language models."}
+    prompt = RenderedPrompt(
+        messages=[RenderedMessage(role="user", content="Write a limerick about large language models.")]
+    )
 
     # populate cache
-    key_HAIKU = llm_HAIKU._llm_cache_key(prompt_kwargs, {})
-    res_HAIKU = llm_HAIKU.generate(prompt_kwargs=prompt_kwargs, llm_kwargs={})
-    key_SONNET = llm_SONNET._llm_cache_key(prompt_kwargs, {})
-    res_SONNET = llm_SONNET.generate(prompt_kwargs=prompt_kwargs, llm_kwargs={})
+    key_HAIKU = llm_HAIKU._llm_cache_key(prompt, {})
+    res_HAIKU = llm_HAIKU.generate(prompt=prompt, llm_kwargs={})
+    key_SONNET = llm_SONNET._llm_cache_key(prompt, {})
+    res_SONNET = llm_SONNET.generate(prompt=prompt, llm_kwargs={})
 
     # check proper cached results
     assert cache.get(key_HAIKU).get("result")["output"] == res_HAIKU
-    assert cache.get(key_HAIKU).get("prompt_kwargs") == prompt_kwargs
+    assert cache.get(key_HAIKU).get("prompt") == prompt
     assert cache.get(key_HAIKU).get("llm_kwargs") == {}
     assert cache.get(key_HAIKU).get("model_name") == BedrockModels.CLAUDE_3_HAIKU.value.name
     assert cache.get(key_SONNET).get("result")["output"] == res_SONNET
-    assert cache.get(key_SONNET).get("prompt_kwargs") == prompt_kwargs
+    assert cache.get(key_SONNET).get("prompt") == prompt
     assert cache.get(key_SONNET).get("llm_kwargs") == {}
     assert cache.get(key_SONNET).get("model_name") == BedrockModels.CLAUDE_3_SONNET.value.name
 
@@ -115,9 +130,11 @@ def test_cached_bedrock_different_models(tmp_path: Path):
 
 def test_metadata():
     llm = Bedrock(BedrockModels.CLAUDE_3_HAIKU)
-    prompt_kwargs = {"prompt": "Write a limerick about large language models."}
+    prompt = RenderedPrompt(
+        messages=[RenderedMessage(role="user", content="Write a limerick about large language models.")]
+    )
 
-    res = llm.generate_metadata(prompt_kwargs=prompt_kwargs, llm_kwargs={})
+    res = llm.generate_metadata(prompt=prompt, llm_kwargs={})
 
     assert "output" in res
     assert "wall_latency" in res

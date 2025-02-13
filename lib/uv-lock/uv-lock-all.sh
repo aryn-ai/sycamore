e4de3273c0619f7014a35af63ac87041f9d4151c
Karan Sampath <176953591+karanataryn@users.noreply.github.com>
2025-02-13 09:31:43 -0800

    updates

--- lib/uv-lock/uv-lock-all.sh
+++ lib/uv-lock/uv-lock-all.sh
@@ -9,8 +9,9 @@ fail() {
 }
 [[ -f "${root}/lib/sycamore/pyproject.toml" ]] || fail "run in root git directory"
 echo "Setting in-project virtualenvs locally"
-poetry config virtualenvs.in-project true --local
-poetry install
+uv venv
+source .venv/bin/activate
+uv sync
 [[ -d .venv ]] || fail "Did not get .venv directory after install"
 
 tomls=$(find . ! -path "**/site-packages/**/*" -name pyproject.toml)
@@ -27,7 +28,7 @@ for i in ${tomls}; do
         (
             echo "--------------------- special casing in $i"
             cd $(dirname "$i")
-            poetry lock --no-update || fail "broke on special case 'poetry lock --no-update' for $i"
+            uv lock || fail "broke on special case 'uv lock' for $i"
             # Do not apply the consistency logic, it can't do anything useful.
         ) || fail "broke on special case for $i"
         continue
@@ -35,9 +36,9 @@ for i in ${tomls}; do
     (
         echo "--------------------- processing in $i"
         cd $(dirname "$i")
-        poetry lock --no-update || fail "broke on regular case 'poetry lock --no-update' $i"
-        poetry install 2>&1 | tee /tmp/poetry-install.out || fail "broke on 'poetry install' for $i"
-        perl -ne 'print qq{$1 = "$2"\n} if /Downgrading (\S+) \((\S+) ->/o;' </tmp/poetry-install.out >/tmp/downgraded
+        uv lock|| fail "broke on regular case 'uv lock' $i"
+        uv sync  2>&1 | tee /tmp/uv-sync.out || fail "broke on 'uv sync' for $i"
+        perl -ne 'print qq{$1 = "$2"\n} if /Downgrading (\S+) \((\S+) ->/o;' </tmp/uv-sync.out >/tmp/downgraded
         cat /tmp/downgraded
         [[ $(wc -l </tmp/downgraded) -eq 0 ]] || fail "broke on downgrading $i it seems these packages are incompatible"
     ) || fail "broke on regular case $i"

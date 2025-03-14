40ed1139e2d6c1927bf5d81b99177a51204935a6
Dhruv Kaliraman <dhruvkal@aryn.ai>
2025-03-14 11:50:28 -0700

    Add TODO

--- lib/sycamore/sycamore/llms/prompts/default_prompts.py
+++ lib/sycamore/sycamore/llms/prompts/default_prompts.py
@@ -422,7 +422,8 @@ class EntityExtractorMessagesPrompt(SimplePrompt):
                 f'"{field}" to answer the question: '
             )
 
-
+# TODO: Need to separate the condition when use_elements is True but the field is a property that does not require chunking.
+#       Could save up on time by not scheduling llm calls.
 LlmFilterMessagesJinjaPrompt = JinjaPrompt(
     system="You are a helpful classifier that generously filters database entries based on questions.",
     user=(

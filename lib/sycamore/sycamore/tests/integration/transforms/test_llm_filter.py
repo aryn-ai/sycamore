e681030b3a8518e81bd41974de23ebe6a63bffbf
Henry Lindeman <hmlindeman@yahoo.com>
2025-04-16 18:09:33 -0700

    add test filter in
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>

--- lib/sycamore/sycamore/tests/integration/transforms/test_llm_filter.py
+++ lib/sycamore/sycamore/tests/integration/transforms/test_llm_filter.py
@@ -9,10 +9,18 @@ def test_llm_filter_ntsb_temp_q():
     ds = ctx.read.materialize(TEST_DIR / "resources/data/materialize/llmfilter-ntsb-temp")
     llm = OpenAI(OpenAIModels.GPT_4O_MINI)
     prompt = LlmFilterMessagesJinjaPrompt.fork(filter_question="Is the temperature less than 60F?")
-    ds = ds.llm_filter(
+    ds_none = ds.llm_filter(
         llm=llm,
         new_field="_autogen_LLMFilterOutput",
         prompt=prompt,
         use_elements=True,
     )
-    assert ds.count() == 0
+    assert ds_none.count() == 0
+
+    ds_one = ds.llm_filter(
+        llm=llm,
+        new_field="_autogen_LLMFilterOutput",
+        prompt=prompt.fork(filter_question="Is the temperature greater than 60F?"),
+        use_elements=True,
+    )
+    assert ds_one.count() == 1

845953fc36ef455020c7ac3387dc09c4de8ac1ac
Soeb Hussain <soeb@ip-172-31-9-237>
2025-04-30 19:15:30 +0000

    adding llms of gpt4.1 family

--- lib/sycamore/sycamore/llms/openai.py
+++ lib/sycamore/sycamore/llms/openai.py
@@ -59,8 +59,11 @@ class OpenAIModels(Enum):
         name="gpt-4o-2024-08-06", is_chat=True
     )  # remove after october 2nd, gpt-4o will point to this model then
     GPT_4O_MINI = OpenAIModel(name="gpt-4o-mini", is_chat=True)
-    GPT_4_1_MINI = OpenAIModel(name="gpt-4.1-mini", is_chat=True)
     GPT_3_5_TURBO_INSTRUCT = OpenAIModel(name="gpt-3.5-turbo-instruct", is_chat=False)
+    GPT_4_1 = OpenAIModel(name="gpt-4.1", is_chat=True)
+    GPT_4_1_MINI = OpenAIModel(name="gpt-4.1-mini", is_chat=True)
+    GPT_4_1_NANO = OpenAIModel(name="gpt-4.1-nano", is_chat=True)
+
 
     @classmethod
     def from_name(cls, name: str):

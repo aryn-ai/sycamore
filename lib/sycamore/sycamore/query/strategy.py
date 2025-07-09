ade5f11c90f6e9bab60ef7bcf0989a2e0b96100d
Henry Lindeman <hmlindeman@yahoo.com>
2025-07-08 17:24:27 -0700

    add limit llm operations and require query database processors
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>

--- lib/sycamore/sycamore/query/strategy.py
+++ lib/sycamore/sycamore/query/strategy.py
@@ -202,7 +202,7 @@ class AlwaysSummarize(LogicalPlanProcessor):
             inputs=[prev_result],
             question=question,
         )
-        return plan
+        return LogicalPlan.model_validate(plan)
 
 
 class OnlyRetrieval(LogicalPlanProcessor):
@@ -229,6 +229,70 @@ class OnlyRetrieval(LogicalPlanProcessor):
             del plan.nodes[plan.result_node]
             plan.result_node = penultimate
             n = plan.nodes[plan.result_node]
+        return LogicalPlan.model_validate(plan)
+
+
+class LimitLlmOperations(LogicalPlanProcessor):
+    """
+    Add a limit node before certain operators. This is useful to make some queries faster, although
+    less accurate.
+
+    Args:
+        types: List of node types to limit; e.g. "LlmFilter", "SummarizeData"
+        limit: How many documents to limit to
+        message: Optional message to add to the description of the added limit node. Default is
+            "Limit the number of documents going through the following llm operation for interactivity"
+    """
+
+    def __init__(self, types: list[str], limit: int, message: Optional[str] = None):
+        self._types = types
+        self._limit = limit
+        self._message = (
+            message or "Limit the number of documents going through the following llm operation for interactivity"
+        )
+
+    def __call__(self, plan: LogicalPlan, **kwargs) -> LogicalPlan:
+        new_nodes: dict[int, Node] = {}
+        new_nodes_i = 0
+        old_to_new_mapping: dict[int, int] = {}
+        for old_i in sorted(plan.nodes.keys()):
+            node = plan.nodes[old_i]
+            new_inputs = [old_to_new_mapping[inp] for inp in node.inputs]
+            # Don't add a limit after another limit. Note - this is close to but not correct.
+            # The correct behavior should be set the limit to the min of the two that I want.
+            # If I'm doing the subquery-limit-by-field then you can probably double up.
+            if node.node_type in self._types and all(new_nodes[nwinp].node_type != "Limit" for nwinp in new_inputs):
+                limit = Limit(
+                    node_type="Limit",
+                    num_records=self._limit,
+                    inputs=new_inputs,
+                    description=self._message,
+                    node_id=new_nodes_i,
+                )
+                node.inputs = [new_nodes_i]
+                new_nodes[new_nodes_i] = limit
+                new_nodes_i += 1
+
+                node.node_id = new_nodes_i
+                new_nodes[new_nodes_i] = node
+                old_to_new_mapping[old_i] = new_nodes_i
+                new_nodes_i += 1
+            else:
+                node.inputs = new_inputs
+                node.node_id = new_nodes_i
+                new_nodes[new_nodes_i] = node
+                old_to_new_mapping[old_i] = new_nodes_i
+                new_nodes_i += 1
+        plan.nodes = new_nodes
+        plan.result_node = old_to_new_mapping[plan.result_node]
+        return LogicalPlan.model_validate(plan)
+
+
+class RequireQueryDatabase(LogicalPlanProcessor):
+    def __call__(self, plan: LogicalPlan, **kwargs) -> LogicalPlan:
+        assert (
+            plan.nodes[0].node_type == "QueryDatabase"
+        ), "Found non-QueryDatabase start node, but QueryDatabase is required"
         return plan
 
 

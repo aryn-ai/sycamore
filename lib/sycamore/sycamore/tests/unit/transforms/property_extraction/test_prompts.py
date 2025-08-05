26d9cac6a0d3cc4a7c2d5b6bfe2d488acdb1846b
Henry Lindeman <hmlindeman@yahoo.com>
2025-08-05 11:19:19 -0700

    add test for format_schema and fix it
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>

---
+++ lib/sycamore/sycamore/tests/unit/transforms/property_extraction/test_prompts.py
@@ -0,0 +1,55 @@
+from sycamore.transforms.property_extraction.prompts import format_schema_v2
+from sycamore.schema import (
+    IntProperty,
+    SchemaV2,
+    NamedProperty,
+    ObjectProperty,
+    ArrayProperty,
+    StringProperty,
+    ChoiceProperty,
+)
+
+complicated_schema = SchemaV2(
+    properties=[
+        NamedProperty(name="a", type=StringProperty(extraction_instructions="extract the letter a")),
+        NamedProperty(
+            name="b",
+            type=ObjectProperty(
+                properties=[
+                    NamedProperty(name="c", type=ChoiceProperty(choices=["1", "2", "3"])),
+                    NamedProperty(
+                        name="d",
+                        type=ArrayProperty(item_type=StringProperty(required=True, description="thing in array")),
+                    ),
+                    NamedProperty(
+                        name="e",
+                        type=ArrayProperty(
+                            item_type=ArrayProperty(item_type=IntProperty(required=True, examples=[1, 4, 9]))
+                        ),
+                    ),
+                ]
+            ),
+        ),
+    ]
+)
+
+expected_formatted_schema = """\
+{
+  a: { type: string, extraction_instructions: extract the letter a },
+  b: object {
+    c: enum { "1", "2", "3" },
+    d: array [
+      { type: string, description: thing in array }
+    ],
+    e: array [
+      array [
+        { type: int, examples: [1, 4, 9] }
+      ]
+    ]
+  }
+}"""
+
+
+def test_format_schema():
+    f = format_schema_v2(complicated_schema)
+    assert f == expected_formatted_schema

bfd8bfc8907518d5439f2a130e302222d9f14999
Alex Meyer <144723289+alexaryn@users.noreply.github.com>
2025-09-26 16:18:05 -0700

    Reduce imports to reduce import time. (#1483)
    * Reduce imports to reduce import time.
    
    * lint

--- lib/sycamore/sycamore/llms/llms.py
+++ lib/sycamore/sycamore/llms/llms.py
@@ -1,26 +1,19 @@
 import inspect
 from abc import ABC, abstractmethod
 import copy
-from enum import Enum
 import logging
 import pickle
 import base64
 from PIL import Image
 from typing import Any, Optional
 
-from sycamore.llms.config import LLMModel
+from sycamore.llms.config import LLMMode, LLMModel
 from sycamore.utils.cache import Cache
 from sycamore.utils.thread_local import ThreadLocalAccess, ADD_METADATA_TO_OUTPUT
 from sycamore.data.metadata import add_metadata
 from sycamore.llms.prompts import RenderedPrompt, RenderedMessage
 
 from sycamore.utils.deprecate import deprecated
-
-
-class LLMMode(Enum):
-    SYNC = 1
-    ASYNC = 2
-    BATCH = 3
 
 
 class LLM(ABC):

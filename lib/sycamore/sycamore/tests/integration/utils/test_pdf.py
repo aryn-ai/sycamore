04f4c81f3f18ae986e1f5dd1a1d8234ba3bbc0e3
Alex Meyer <144723289+alexaryn@users.noreply.github.com>
2025-10-02 20:01:43 -0700

    PDF-to-image iterator that cleans up if interrupted. (#1488)
    * PDF-to-image iterator that cleans up if interrupted.
    
    * Enforce use as context manager.
    
    * Accept pdf_path as Path or str.
    
    * Address deadlock race.  Move to integration test.
    
    * No more threads; non-blocking reads, memoryviews and BytesIO
    
    * Duplicated code to class; unbuffer streams; test each image.
    
    * Refactoring and renaming based on PR feedback.

---
+++ lib/sycamore/sycamore/tests/integration/utils/test_pdf.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import logging
+from tempfile import TemporaryDirectory
+
+from PIL import Image
+
+from sycamore.tests.config import TEST_DIR
+from sycamore.utils.pdf import PdfToImageFiles
+
+
+def test_basic():
+    fn = TEST_DIR / "resources/data/pdfs/Ray.pdf"
+    with TemporaryDirectory(prefix="test_pdf") as dir:
+        n = full(fn, dir)
+        assert n == 17
+        bail(fn, dir, 8)
+        with os.scandir(dir) as scan:
+            for ent in scan:
+                assert False, "temp dir should be empty now"
+
+
+def full(fn: str, dir: str) -> int:
+    cnt = 0
+    with PdfToImageFiles(pdf_path=fn, file_dir=dir) as gen:
+        for p in gen:
+            with Image.open(p) as img:
+                img.load()  # verify file is an uncorrupted image
+            p.unlink()
+            cnt += 1
+    return cnt
+
+
+def bail(fn: str, dir: str, limit: int) -> None:
+    try:
+        with PdfToImageFiles(pdf_path=fn, file_dir=dir) as gen:
+            cnt = 0
+            for p in gen:
+                logging.info(f"{cnt} {p}")
+                with Image.open(p) as img:
+                    img.load()  # verify
+                p.unlink()
+                cnt += 1
+                if cnt > limit:
+                    raise RuntimeError("bail out")
+        assert False, "should not reach here"
+    except RuntimeError as e:
+        logging.info(str(e))
+
+
+def main(args=None) -> int:
+    if args is None:
+        args = sys.argv[1:]
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(levelname).1s %(asctime)s %(message)s",
+        datefmt="%Y%m%d_%H%M%S",
+    )
+    fn = args.pop(0)
+    with TemporaryDirectory(prefix="test_pdf") as dir:
+        n = full(fn, dir)
+        logging.info(f"total_pages={n}")
+        bail(fn, dir, n // 2)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())

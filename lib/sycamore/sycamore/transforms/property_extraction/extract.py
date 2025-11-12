ede0d7a55eae31d01e6dc78228f1a47edfa07e73
Austin Lee <austin@aryn.ai>
2025-11-12 12:33:28 -0800

    Add retries on schema extraction LLM calls (#1509)
    * Add retries on schema extraction LLM calls
    
    * Retry on invalid SchemaV2
    
    * Fix unit tests
    
    * Address comment

--- lib/sycamore/sycamore/transforms/property_extraction/extract.py
+++ lib/sycamore/sycamore/transforms/property_extraction/extract.py
@@ -3,6 +3,8 @@ import asyncio
 import logging
 import json
 
+from pydantic import ValidationError
+
 from sycamore.data.document import Document, Element
 from sycamore.plan_nodes import Node
 from sycamore.schema import NamedProperty, ObjectProperty, SchemaV2 as Schema, DataType
@@ -320,9 +322,24 @@ class SchemaExtract(MapBatch):
         result_dict = dict()
 
         async def do_one_step(elements, document):
-            rendered = self._prompt.render_multiple_elements(elements, document)
-            result = await self._llm.generate_async(prompt=rendered)
-            return {ii["name"]: ii for ii in extract_json(result)}
+            max_retries = 2  # Up to 3 calls
+            retries = 0
+            while retries <= max_retries:
+                try:
+                    rendered = self._prompt.render_multiple_elements(elements, document)
+                    result = await self._llm.generate_async(prompt=rendered)
+                    extracted = extract_json(result)
+                    for p in extracted:
+                        NamedProperty.model_validate(p)
+                    candidate = {ii["name"]: ii for ii in extracted}
+                    return candidate
+                except (ValueError, ValidationError) as exc:
+                    _logger.exception(exc)
+                    if retries >= max_retries:
+                        _logger.warning("All retries failed, returning empty schema.")
+                        break
+                    retries += 1
+            return {}
 
         results = await asyncio.gather(
             *(do_one_step(elements, document) for elements in self._step_through.step_through(document))
